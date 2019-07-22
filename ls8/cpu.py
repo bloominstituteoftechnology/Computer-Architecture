b240a94efbc9142283d4de8de87fe642729a009b
Jor_Poon <jormpoon@gmail.com>
2019-07-22 18:48:12 -0400

    created run function for cpu

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -8,9 +8,14 @@ class CPU:
     def __init__(self):
         """Construct a new CPU."""
         self.reg = [0] * 0x08
+        # holds 255 bytes
         self.ram = [0] * 0xff
         #Program Counter, address of the currently executing instruction
         self.pc = 0x00 
+        #Instruction Register, contains a copy of the currently executing instruction
+        self.ir = 0x00
+        #opcodes
+        self.h = 0b00000001 
         
     def ram_read(self, current):
         return self.ram[current]
@@ -60,7 +65,7 @@ class CPU:
             #self.fl,
             #self.ie,
             self.ram_read(self.pc),
-            self.ram_read(self.pc + 1),
+            self.ram_read(self.pc + 3),
             self.ram_read(self.pc + 2)
         ), end='')
 
@@ -71,4 +76,44 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        LDI = 0b10000010
+        PRN = 0b01000111
+        
+        run_cpu = True
+
+        while run_cpu:
+
+            self.ir = self.pc
+            o1 = self.ram_read(self.pc + 1)
+            o2 = self.ram_read(self.pc + 2)
+
+            if self.ram[self.ir] == self.h:
+                run_cpu = False
+            elif self.ram[self.ir] == LDI:
+                self.reg[o1] = o2
+                self.pc += 3
+            elif self.ram[self.ir] == PRN:
+                print(self.reg[o1])
+                self.pc += 2
+
+
+        # this prints out 0
+
+        # run_cpu = True
+
+        # while run_cpu:
+
+        #     self.ir = self.pc
+        #     o1 = self.ram_read(self.pc + 1)
+        #     o2 = self.ram_read(self.pc + 2)
+
+        #     if self.ram[self.ir] == LDI:
+        #         self.reg[o1] == o2
+        #         self.pc += 3
+        #     elif self.ram[self.ir] == PRN:
+        #         print(self.reg[o1])
+        #         self.pc += 2
+        #     elif self.ram[self.ir] == self.h:
+        #         run_cpu = False
+
+
