3de0fc4c47bb5e764f4db2e188f4a7a95138000e
Sharanjit Sandhu <sharanjit.sandhuu@gmail.com>
2019-08-20 15:31:32 -0700

    Added run function

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -5,25 +5,27 @@ import sys
 
 class CPU:
     """Main CPU class."""
+
+    def __init__(self):
+        """Construct a new CPU."""
     #  Add list properties to the `CPU` class to hold 256 bytes of memory
     # and 8 general-purpose registers.
-    self.memory = [0] * 256  # memory is a list of 256 zeroes
-    self.register = [0] * 8
+        self.memory = [0] * 256  # memory is a list of 256 zeroes
+        self.register = [0] * 8
+        self.pc = 0
 
     # In `CPU`, add method `ram_read()` and `ram_write()`
     # that access the RAM inside the `CPU` object.
 
-    # `ram_read()` should accept the address to read and return the value stored there.
+    """`ram_read()` should accept the address to read and return the value stored there."""
+
     def ram_read(self, address):
         return self.memory[address]
 
-    # `ram_write()` should accept a value to write, and the address to write it to.
+    """`ram_write()` should accept a value to write, and the address to write it to."""
+
     def ram_write(self, value, address):
         self.memory[address] = value
-
-    def __init__(self):
-        """Construct a new CPU."""
-        pass
 
     def load(self):
         """Load a program into memory."""
@@ -77,4 +79,11 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        # It needs to read the memory address that's stored in register `PC`,
+        # and store that result in `IR`, the _Instruction Register_.
+        # This can just be a local variable in `run()`.
+        IR = self.memory[self.pc]
+        # Using `ram_read()`, read the bytes at `PC+1` and `PC+2` from RAM into variables
+        # `operand_a` and `operand_b` in case the instruction needs them.
+        operand_a = ram_read(IR + 1)
+        operand_b = ram_read(IR + 2)
