ec63d2ed5367ac068fc16aed9b576dbfb57e1233
Sharanjit Sandhu <sharanjit.sandhuu@gmail.com>
2019-08-21 15:50:51 -0700

    Implemented yje load func to load filesa and implemented a Multiply instruction

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -27,33 +27,48 @@ class CPU:
     def ram_write(self, value, address):
         self.ram[address] = value
 
-    def load(self):
+    def load(self, argv):
         """Load a program into memory."""
 
         address = 0
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010,  # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111,  # PRN R0
-            0b00000000,
-            0b00000001,  # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010,  # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111,  # PRN R0
+        #     0b00000000,
+        #     0b00000001,  # HLT
+        # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
+
+        try:
+            # sys.argv is a list in Python, which contains the command-line arguments passed to the script.
+            with open(sys.argv[1]) as f:
+                for line in f:
+                    if line[0].startswith('0') or line[0].startswith('1'):
+                        num = line.split('#')[0].strip()
+                        self.ram[address] = int(num, 2)
+                        address += 1
+        except FileNotFoundError:
+            print(f"{sys.argv[0]}: {sys.argv[1]} Not found")
+            sys.exit(2)
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
+            # self.reg[reg_a] += self.reg[reg_b]
+            self.register[reg_a] += self.register[reg_b]
         # elif op == "SUB": etc
+        elif op == "MUL":
+            self.register[reg_a] *= self.register[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -83,6 +98,7 @@ class CPU:
         LDI = 0b10000010
         PRN = 0b01000111
         HLT = 0b00000001
+        MUL = 0b10100010
 
         running = True
 
@@ -106,6 +122,17 @@ class CPU:
                 self.pc += 2
             elif IR == HLT:
                 running = False
+            elif IR == MUL:
+                self.alu("MUL", operand_a, operand_b)
+                self.pc += 3
             else:
                 print(f"Unknown instruction!!{self.ram[self.pc]} ")
                 sys.exit(1)
+
+
+# RUN in terminal as:-----------------
+# python3 ls8.py examples/print8.ls8
+# prints: 8
+# python3 ls8.py examples/mult.ls8
+# prints: 72
+# --------------------------------------
