d58f17ea5f243e0767acb628f0c118d25d8fca6d
Sem8 <mssemmi8@gmail.com>
2019-08-21 01:57:51 -0400

    step 7 of day 2 assignment complete

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -30,20 +30,25 @@ class CPU:
         address = 0
 
         # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
+        with open(sys.argv[1]) as f:
+            for line in f:
+                if line[0] != '#' and line != '\n':
+                    self.ram[address] = int(line[0:8], 2)
+                    address += 1
+            f.closed
+        print(self.ram)
 
 
     def alu(self, op, reg_a, reg_b):
