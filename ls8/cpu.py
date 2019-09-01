40b9dc40a1d886acf12cba692295bbf577289c4e
Phinehas <kohler.nai@gmail.com>
2019-09-01 16:55:28 -0600

    added mul command

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -24,22 +24,6 @@ class CPU:
         """Load a program into memory."""
 
         address = 0
-
-        # # For now, we've just hardcoded a program:
-
-        # program = [
-        #     # From print8.ls8
-        #     0b10000010,  # LDI R0,8
-        #     0b00000000,
-        #     0b00001000,
-        #     0b01000111,  # PRN R0
-        #     0b00000000,
-        #     0b00000001,  # HLT
-        # ]
-
-        # for instruction in program:
-        #     self.ram[address] = instruction
-        #     address += 1
         pattern = "[0-1]{8}"
         if len(sys.argv) >= 2:
             try:
@@ -49,7 +33,7 @@ class CPU:
                     for line in lines:
                         match = re.match(pattern, line)
                         if match:
-                            self.ram[address] =int(line[0:8], base = 2)
+                            self.ram[address] = int(line[0:8], base=2)
                             address += 1
             except FileNotFoundError:
                 print("File not found, check the name given")
@@ -60,10 +44,11 @@ class CPU:
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
-
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        # elif op == "SUB": etc
+        elif op == "MUL":
+            mul = self.reg[reg_a] * self.reg[reg_b]
+            self.reg[reg_a] = mul
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -93,6 +78,7 @@ class CPU:
         LDI = 0b10000010
         PRN = 0b01000111
         HLT = 0b00000001
+        MUL = 0b10100010
 
         running = True
 
@@ -109,6 +95,9 @@ class CPU:
             elif IR == PRN:
                 print(self.reg[operand_a])
                 self.pc += 2
+            elif IR == MUL:
+                self.alu("MUL", operand_a, operand_b)
+                self.pc += 3
             elif IR == HLT:
                 running = False
             else:
