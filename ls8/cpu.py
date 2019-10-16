b7624f2e4f9604d48fba35d497a7b4a97f9d7ac8
Quinn Dougherty <quinn.dougherty92@gmail.com>
2019-10-16 16:54:49 -0400

    finished wednesday mvp

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -16,15 +16,24 @@ class CPU:
 
         # internal registers
         self.pc = 0 # PC: Program Counter, address of the currently executing instruction
-        self.ir = 0 # IR: Instruction Register, contains a copy of the currently executing instruction
-        self.mar = 0 # MAR: Memory Address Register, holds the memory address we're reading or writing
-        self.mdr = 0 # MDR: Memory Data Register, holds the value to write or the value just read
-        self.fl = 0 # FL: Flags, see below
-
-        self.ram = [0b0] * 255
+        self.ir = None # IR: Instruction Register, contains a copy of the currently executing instruction
+        self.mar = None # MAR: Memory Address Register, holds the memory address we're reading or writing
+        self.mdr = None # MDR: Memory Data Register, holds the value to write or the value just read
+        self.fl = None # FL: Flags, see below
+        self.spl = None # stack pointer location
+        #
+        self.ram = [0b0] * 0xFF
+        self.spl = 8 - 1
+        self.registers[self.spl] = 0xF4
 
         # opcodes
-        self.OPCODES = {0b10000010: 'LDI', 0b01000111: 'PRN', 0b00000001: 'HLT', 0b10100010: 'MUL'}
+        self.OPCODES = {0b10000010: 'LDI',
+                        0b01000111: 'PRN',
+                        0b00000001: 'HLT',
+                        0b10100010: 'MUL',
+                        0b01000110: 'POP',
+                        0b01000101: 'PUSH',
+        }
 
     def load(self, filename: str):
         """Load a program into memory."""
@@ -81,9 +90,10 @@ class CPU:
         """Run the CPU."""
         running = True
         while running:
-            command = self.ram[self.pc]
+            #self.trace()
+            self.ir = self.ram[self.pc]
             try:
-                op = self.OPCODES[command]
+                op = self.OPCODES[self.ir]
                 # do LDI
                 if op == 'LDI':
                     reg = self.ram[self.pc+1]
@@ -95,7 +105,7 @@ class CPU:
                 elif op == 'PRN':
                     reg = self.ram[self.pc+1]
                     val = self.registers[reg]
-                    print(val)
+                    print(f"{val:x}")
                     self.pc += 2
 
                 # pass to alu
@@ -104,13 +114,39 @@ class CPU:
                     reg_b = self.ram[self.pc+2]
                     self.alu(op, reg_a, reg_b)
                     self.pc += 3
+
+                # push
+                elif op == 'PUSH':
+
+                    reg = self.ram[self.pc + 1]
+                    val = self.registers[reg]
+                    #if val in self.OPCODES.keys():
+                    #    print("hi")
+                    #Decrement the SP.
+                    self.registers[self.spl] -= 1
+                    # Copy the value in the given register to the address pointed to by SP.
+                    self.ram[self.registers[self.spl]] = val
+                    self.pc += 2
+
+                # pop
+                elif op == 'POP':
+                    reg = self.ram[self.pc + 1]
+                    val = self.ram[self.registers[self.spl]]
+                    # Copy the value from the address pointed to by SP to the given register.
+                    self.registers[reg] = val
+                    # Increment SP.
+                    self.registers[self.spl] += 1
+                    self.pc += 2
+
+
                 # exit
                 elif op == 'HLT':
                     running = False
                     # self.pc += 1 # i don't know if it makes sense to do this.
 
+
             except KeyError as e:
-                print(f"unknown command {command}")
+                print(f"unknown command {self.ir}")
                 self.pc += 1
         pass
 
