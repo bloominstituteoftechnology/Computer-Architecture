f254329967ba7234987f3d6fbc64473d5c3b66e6
ghungpham <pham.ghungj@gmail.com>
2019-10-16 18:23:49 -0400

    added

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -8,6 +8,7 @@ import sys
 HLT = 0b00000001
 LDI = 0b10000010
 PRN = 0b01000111
+MUL = 0b10100010
 
 
 
@@ -25,28 +26,50 @@ class CPU:
         self.IR = None
         ##MAR: Memory Address Register, holds the memory address we're reading or writing
         ##MDR: Memory Data Register, holds the value to write or the value just read
-
+        ##self.branchtable = {LDI: self.handle_LDI, PRN : self.handle_PRN, MUL: self.handle_MUL}
 
     def load(self):
         """Load a program into memory."""
+    
+        try:
+            with open(sys.argv[1], 'r') as f:
+                #ignore all the #instructions and #blank line
+                lines = [line for line in f.readlines() if not (line[0]=='#' or line[0]=='\n')]
+                program = [int(line.split('#')[0].strip(), 2) for line in lines]
+                print(program)
 
-        address = 0
+            address = 0
 
-        # For now, we've just hardcoded a program:
+            for instruction in program:
+                self.ram[address] = instruction
+                address += 1
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        
+        except FileNotFoundError:
+            print(f"{sys.argv[0]}: {sys.argv[1]} not found")
+            sys.exit(2)
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+
+        
+
+
+        # address = 0
+
+        # # For now, we've just hardcoded a program:
+
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
+
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
 
 
     def alu(self, op, reg_a, reg_b):
@@ -55,6 +78,10 @@ class CPU:
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         #elif op == "SUB": etc
+
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
+
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -65,12 +92,12 @@ class CPU:
         """
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
-            self.pc,
+            self.PC,
             #self.fl,
             #self.ie,
-            self.ram_read(self.pc),
-            self.ram_read(self.pc + 1),
-            self.ram_read(self.pc + 2)
+            self.ram_read(self.PC),
+            self.ram_read(self.PC + 1),
+            self.ram_read(self.PC + 2)
         ), end='')
 
         for i in range(8):
@@ -83,12 +110,12 @@ class CPU:
         running = True
 
         while running:
-            address = self.PC
+            address= self.PC
             command = self.ram[address]
-            self.IR = command
 
             ##HALT COMMAND
             if command == HLT:
+                print('HLT == EXIT')
                 running = False
 
             ##LDI register immediate
@@ -97,19 +124,31 @@ class CPU:
             elif command == LDI:
                 register = self.ram[self.PC + 1]
                 integer = self.ram[self.PC + 2]
-                print("LDI register, integer", register, integer)
+                print(f"LDI integer {integer} at reg {register}")
                 self.reg[register] = integer
+                self.PC += 3
 
 
             ##Print numeric value stored in the given register.
             elif command == PRN:
                 register = self.ram[self.PC + 1]
-                print(self.reg[register])
+                print(f'Result: {self.reg[register]}')
+                self.PC += 2
+
+            
+            elif command == MUL:
+                reg_a = self.ram[self.PC + 1]
+                reg_b = self.ram[self.PC + 2]
+                result = self.alu("MUL", reg_a, reg_b)
+                reg_a = result
+                self.PC += 3
 
 
+            else:
+                print(f"unknown instruction {command}")
+                self.PC += 1
 
-
-            address += 1
+          
 
 
 
