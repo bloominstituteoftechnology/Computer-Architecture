9296ab5c52d4d6eca4ef5bf3c8dc3084bc677c3a
PetejStone <petejstoney@gmail.com>
2019-11-13 14:30:35 -0800

    reading lines from files passed in as arg

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -19,19 +19,14 @@ class CPU:
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        program = sys.argv[1]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        with open(program) as f:
+            for line in f:
+                print(line)
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
 
 
     def alu(self, op, reg_a, reg_b):
