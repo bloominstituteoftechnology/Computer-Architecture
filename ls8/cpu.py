b6b39f310d89581bb78e552f5c3a01bb7b1a28cc
NikuX <44727534+NikuX@users.noreply.github.com>
2019-12-17 21:30:54 +0000

    first commit

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,8 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.reg = [0] * 8
+        self.pc = 0
 
     def load(self):
         """Load a program into memory."""
