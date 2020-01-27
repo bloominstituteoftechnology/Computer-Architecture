c246c292e570e8c75ca8fed2e168b35d9cee07e9
luis guzman <antilou86@gmail.com>
2020-01-27 15:52:53 -0500

    added instruction variables to run() function, added instuction register, oprand_a and operand_b to run() as well.

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -5,10 +5,12 @@ import sys
 class CPU:
     """Main CPU class."""
 
-    def __init__(self, ram):
+    def __init__(self):
         """Construct a new CPU."""
-        self.ram = ram
-        
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0
+
     def load(self):
         """Load a program into memory."""
 
@@ -60,6 +62,20 @@ class CPU:
 
         print()
 
+    def ram_read(self, count):
+        return self.ram[count]
+
+    def ram_write(self, value, address):
+        self.ram[address] = value
+
     def run(self):
         """Run the CPU."""
+        LDI = 0b10000010
+        PRN = 0b01000111
+        HLT = 0b00000001
+
+        IR = self.ram_read(self.pc)
+        operand_a = self.ram_read(self.pc+1)
+        operand_b = self.ram_read(self.pc+2)
+
         pass
