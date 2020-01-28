5d0f4c8ac9fb6948a56cbb98b4d239268810e6ec
luis guzman <antilou86@gmail.com>
2020-01-28 15:30:18 -0500

    MVP for day 2 complete

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -13,25 +13,34 @@ class CPU:
 
     def load(self):
         """Load a program into memory."""
-
         address = 0
-
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
+        program = []
+        #check sys args for a filename
+        if len(sys.argv) != 2:
+            print(f"usage: file.py filename", file=sys.stderr)
+            sys.exit(1)
+        try:
+            #open the file,
+            with open(sys.argv[1]) as f:
+                for line in f:
+                    #filter out spaces and comments.
+                    comment_split = line.strip().split("#")
+                    num = comment_split[0]
+                    #handles blank spaces
+                    if num == "":
+                        continue
+                    #force integer type
+                    x= int(num, 2)
+                    #load all the bits into a list called program.
+                    program.append(x)
+        except FileNotFoundError:
+            print(f"{sys.argv[0]}: {sys.argv[1]} not found")           
+            sys.exit(2)
+        #close the file
+        f.close     
         for instruction in program:
             self.ram[address] = instruction
             address += 1
-
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -75,28 +84,35 @@ class CPU:
         LDI = 0b10000010
         PRN = 0b01000111
         HLT = 0b00000001
+        MUL = 0b10100010
         IR = self.pc
-        operand_a = self.ram_read(IR+1)
-        operand_b = self.ram_read(IR+2)
+
+
         running = True
 
         while running:
-
+            operand_a = self.ram_read(IR+1)
+            operand_b = self.ram_read(IR+2)
             if self.ram[IR] == LDI:
                 #load operand_b into ram at location ram[operand_a]
                 self.ram_write(operand_a, operand_b)
-                #increment the IR by two
+                #increment the IR by three (the number of instructions per function)
                 IR += 3
-                print(f"ram loaded with the value: {self.ram[operand_a]}")
+                #print(f"ram loaded with value of {self.ram[operand_a]}")
 
             elif self.ram[IR] == PRN:
                 #print the thing and increment.
-                print(f"you requested a print?  {self.ram[self.ram[IR+1]]}")
+                print(f"{self.ram[self.ram[IR+1]]}")
                 IR += 2
-
+            elif self.ram[IR] == MUL:
+                #multiply r0 and r1
+                temp = self.ram_read(operand_a) * self.ram_read(operand_b)
+                print(f"{self.ram_read(operand_a)} times {self.ram_read(operand_b)}")
+                #save the resulting value into r0.
+                self.ram_write(operand_a, temp)
+                IR += 3
             elif self.ram[IR] == HLT:
                 running = False
-                print(f"no longer running. buhbye now.")
             else:
                 print(f"unrecognized input, moving to next cycle")
                 IR += 1 
