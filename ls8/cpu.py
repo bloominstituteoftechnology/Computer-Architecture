88e3772b7ab705aa8ab904ecd58b8fdfc07da2de
Jeff <theta_158@yahoo.com>
2020-02-24 23:34:53 -0500

    completed day 1 cpu.py

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,20 @@
 
 import sys
 
+
+LDI = 0b10000010
+PRN = 0b01000111
+HLT = 0b00000001
+
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.pc = 0
+        self.reg = [0] * 8
+        self.ram = [0] * 256
 
     def load(self):
         """Load a program into memory."""
@@ -18,25 +26,31 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
+    def ram_read(self, mar):
+        mdr = self.ram[mar]
+        return mdr
+
+    def ram_write(self, mdr, mar):
+        self.ram[mar] = mdr
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +62,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -62,4 +76,18 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        while True:
+            opcode = self.ram[self.pc]
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+            if opcode == LDI:
+                self.reg[operand_a] = operand_b
+                self.pc += 3
+            elif opcode == PRN:
+                print(self.reg[operand_a])
+                self.pc += 2
+            elif opcode == HLT:
+                sys.exit(0)
+            else:
+                print(f"I did not understand that command: {opcode}")
+                sys.exit(1)
