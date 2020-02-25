6c5b845c96d04a510a3a127b718a698ce72a6d02
Michael Redig <mredig@gmail.com>
2020-02-25 12:50:45 -0600

    (feat) updated guided project to load from file

--- guided project/simple.py
+++ guided project/simple.py
@@ -7,25 +7,42 @@ SAVE            = 4  # save a value to a register
 PRINT_REGISTER  = 5  # print value in a register
 ADD             = 6  # add 2 registers, store result in 1st
 
-memory = [
-    PRINT_BEEJ,
-    SAVE,  # save `65` in r2
-    65,
-    2,
-    SAVE,  # save `20` in r3
-    20,
-    3,
-    ADD,  # R2 += R3
-    2,
-    3,
-    PRINT_REGISTER,  # print r2 (85)
-    2,
-    HALT
-]
+memory = [0] * 256
+
+if len(sys.argv) != 2:
+    print("Error: Must have file name")
+    sys.exit(1)
+
+def loadMemory(filename):
+    memPointer = 0
+    try:
+        #open file
+        with open(sys.argv[1]) as f:
+            #read all lines
+            for line in f:
+                #ignore comments
+                commentSplit = line.strip().split("#")
+                value = commentSplit[0].strip()
+                #ignore blank lines
+                if value == "":
+                    continue
+                #cast to numbers
+                num = int(value)
+                # print(num)
+
+                memory[memPointer] = num
+                memPointer += 1
+    except FileNotFoundError:
+        print("File not found")
+        sys.exit(2)
+
+loadMemory(sys.argv[1])
 
 register = [0] * 8
 
 pc = 0 # program counter
+
+
 
 while True:
     command = memory[pc]
