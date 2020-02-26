6f3f8a99b3f21485e7c05500c384b48846f35b3c
SamH3pn3r <samuelhepner03@gmail.com>
2020-02-26 14:51:47 -0800

    Day 3 MVP done..working towards Day 4 MVP

--- simple.py
+++ simple.py
@@ -6,6 +6,8 @@ PRINT_NUM      = 3
 SAVE           = 4  # Save a value to a register
 PRINT_REGISTER = 5  # Print the value in a register
 ADD            = 6  # Add 2 registers, store the result in 1st reg
+PUSH           = 7
+POP            = 8
 
 memory = [
     PRINT_BEEJ,
@@ -26,6 +28,7 @@ memory = [
 memory = [0] * 256
 register = [0] * 8
 
+SP = 7
 
 pc = 0 # Program counter
 
@@ -49,7 +52,7 @@ def load_memory(filename):
                 memory[address] = num
                 address += 1
                 
-                print(f"{num:08b}: {num}")
+                # print(f"{num:08b}: {num}")
 
     except FileNotFoundError:
         print("File not found")
@@ -86,6 +89,24 @@ while True:
         reg_b = memory[pc + 2]
         register[reg_a] += register[reg_b]
         pc += 3
+    elif command == PUSH:
+        # Grab the register argument
+        reg = memory[pc + 1]
+        val = register[reg]
+        # Decrement the SP.
+        register[SP] -= 1
+        # Copy the value in the given register to the address pointed to by SP.
+        memory[register[SP]] = val
+        pc += 2
+    elif command == POP:
+        # Grab the value from the top of the stack
+        reg = memory[pc + 1]
+        val = memory[register[SP]]
+        # Copy the value from the address pointed to by SP to the given register.
+        register[reg] = val
+        # Increment SP.
+        register[SP] += 1
+        pc += 2
     elif command == HALT:
         sys.exit(0)
     else:
