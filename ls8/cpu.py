205bad12b519e0f463cbd95b7e8eac6859627600
Jeff <theta_158@yahoo.com>
2020-02-28 13:02:06 -0500

    mvp

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -12,8 +12,17 @@ POP = 0b01000110
 PUSH = 0b01000101
 CALL = 0b01010000
 RET = 0b00010001
+CMP = 0b10100111
+JMP = 0b01010100
+JEQ = 0b01010101
+JNE = 0b01010110
 
 SP = 7
+
+# Flags
+LT = 0b00000100
+GT = 0b00000010
+EQ = 0b00000001
 
 
 class CPU:
@@ -24,6 +33,7 @@ class CPU:
         self.pc = 0
         self.reg = [0] * 8
         self.ram = [0] * 256
+        self.fl = 0
         self.bt = {
             LDI: self.op_ldi,
             PRN: self.op_prn,
@@ -33,7 +43,11 @@ class CPU:
             POP: self.op_pop,
             PUSH: self.op_push,
             CALL: self.op_call,
-            RET: self.op_ret
+            RET: self.op_ret,
+            CMP: self.op_cmp,
+            JMP: self.op_jmp,
+            JEQ: self.op_jeq,
+            JNE: self.op_jne
         }
 
     def op_ldi(self, operand_a, operand_b):
@@ -58,11 +72,35 @@ class CPU:
         self.push_val(self.reg[operand_a])
 
     def op_call(self, operand_a, operand_b):
+        # Pushing next instruction to stack
         self.push_val(self.pc + 2)
+        # Setting pc to address of subroutine
         self.pc = self.reg[operand_a]
 
     def op_ret(self, operand_a, operand_b):
+        # Poping next instruction off top of stack
         self.pc = self.pop_val()
+
+    def op_cmp(self, operand_a, operand_b):
+        self.alu("CMP", operand_a, operand_b)
+
+    def op_jmp(self, operand_a, operand_b):
+        # Set the PC to the address stored in the given register
+        self.pc = self.reg[operand_a]
+
+    def op_jeq(self, operand_a, operand_b):
+        # If equal flag is set (true), jump to the address stored in the given register
+        if self.fl == EQ:
+            self.pc = self.reg[operand_a]
+        else:
+            self.pc += 2
+
+    def op_jne(self, operand_a, operand_b):
+        # If E flag is clear (false, 0), jump to the address stored in the given register
+        if not self.fl == EQ:
+            self.pc = self.reg[operand_a]
+        else:
+            self.pc += 2
 
     def load(self, filename):
         """Load a program into memory."""
@@ -89,10 +127,14 @@ class CPU:
             sys.exit(2)
 
     def ram_read(self, mar):
+        # Accept the address to read (mar) and return
+        # address to write (mdr)
         mdr = self.ram[mar]
         return mdr
 
     def ram_write(self, mdr, mar):
+        # Accept a value to write (mdr), and address
+        # to write it to (mar)
         self.ram[mar] = mdr
 
     def push_val(self, val):
@@ -111,6 +153,13 @@ class CPU:
             self.reg[reg_a] += self.reg[reg_b]
         elif op == "MUL":
             self.reg[reg_a] *= self.reg[reg_b]
+        elif op == "CMP":
+            if self.reg[reg_a] < self.reg[reg_b]:
+                self.fl = LT
+            elif self.reg[reg_a] > self.reg[reg_b]:
+                self.fl = GT
+            else:
+                self.fl = EQ
         # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
@@ -123,7 +172,7 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            # self.fl,
+            self.fl,
             # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
@@ -142,7 +191,7 @@ class CPU:
             operand_a = self.ram_read(self.pc + 1)
             operand_b = self.ram_read(self.pc + 2)
             instruction_length = (instruction >> 6) + 1
-            instruction_set_pc = (instruction >> 4) == 1
+            instruction_set_pc = ((instruction >> 4) & 0b1) == 1
             if instruction in self.bt:
                 self.bt[instruction](operand_a, operand_b)
             # Check if opcode sets the pc
