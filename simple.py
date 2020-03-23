8093e29d5168b88a20b140b60b74f9542aae0415
Justin Gent <justinbgent@gmail.com>
2020-03-23 17:31:57 -0600

    Add first solution to cpu.py

--- simple.py
+++ simple.py
@@ -28,39 +28,40 @@ register = [0] * 8
 pc = 0
 running = True
 
+# represents our processor
 while running:
     command = memory[pc]
-    
+
     if command == PRINT_BEEJ:
         print("Beej!")
         pc += 1
-        
+
     elif command == HALT:
         running = False
         pc += 1
-        
+
     elif command == PRINT_NUM:
         num = memory[pc + 1]
         print(num)
         pc += 2
-        
+
     elif command == SAVE:
         num = memory[pc + 1]
         reg = memory[pc + 2]
         register[reg] = num
         pc += 3
-        
+
     elif command == PRINT_REGISTER:
         reg = memory[pc + 1]
         print(register[reg])
         pc += 2
-        
+
     elif command == ADD:
         reg_a = memory[pc + 1]
         reg_b = memory[pc + 2]
         register[reg_a] += register[reg_b]
         pc += 3
-        
+
     else:
         print(f"Unknown instruction: {command}")
         sys.exit(1)
