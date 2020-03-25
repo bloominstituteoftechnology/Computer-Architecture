3ac67c60b02ccd41d28669469fbc491760cdaed2
Hira Khan <sunshine9.r8@gmail.com>
2020-03-25 16:01:12 -0400

    today's work with push and pop

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -23,24 +23,34 @@ class CPU:
 
     def load(self):
         """Load a program into memory."""
+        try:
+            address = 0
+            # sys.argv[0] is the name of the running program itself
+            filename = sys.argv[1]
+            with open(filename) as f:
+                for line in f:
+                    # ignore comments
+                    comment_split = line.split('#')
+                    # strip out whitespace
+                    num = comment_split[0].strip()
+                    # ignore blank lines
+                    if num == "":
+                        continue
+                    # convert the binary string to integers.
+                    # built-in integer function dose it for us.
+                    value = int(num,2)
 
-        address = 0
+                    self.ram[address] = value
+                    address+=1
 
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        except FileNotFoundError:
+            print(f"{sys.argv[0]}: {sys.argv[1]} not found")
+            sys.exit(2)
+        #
+        #
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
 
 
     def alu(self, op, reg_a, reg_b):
@@ -83,6 +93,14 @@ class CPU:
         self.ram[MAR]=MDR
         # so we will move the stuff from MAR TO MDR
 
+# beautifuly the run function:
+
+    # def run(self):
+    #     ir = HLT
+    #     self.branchtable[ir]
+    #
+    #     ir = LDI
+    #     self.branchtable[ir]
     def run(self):
         """Run the CPU."""
 
@@ -91,7 +109,10 @@ class CPU:
         LDI=0b10000010
         HLT=0b00000001
         PRN=0b01000111
-
+        MUL=0b10100010
+        ADD=0b10100000
+        POP=0b01000110
+        PUSH=0b01000101
         while Running:
             # read the memory address sotred in pc and store it in Instruction Register
             Command=self.ram_read(self.pc)
@@ -110,5 +131,50 @@ class CPU:
             elif Command==PRN:
                 # PRN: adding
                 reg = self.ram[self.pc+1]
-                print(self.reg[reg])  # prints 8 to the console.
+                # print(self.reg[reg])  # prints 8 to the console.
                 self.pc+=2
+
+            elif Command==MUL:
+                register_a = self.ram_read(self.pc+1)
+                register_b = self.ram_read(self.pc+2)
+                # now save the multiplication of these two in register_a
+                self.reg[register_a] = self.reg[register_a]*self.reg[register_b]
+                print(self.reg[register_a])
+                self.pc+=3
+
+            elif Command == ADD:
+                reg_a = self.ram[self.pc+1]
+                reg_b = self.ram[self.pc+2]
+                register[reg_a] += register[reg_b]
+                pc += 3
+
+            # System stack ..
+
+            elif Command == PUSH:
+                # look at the opcode argument
+                # a number from 0 to 7
+                reg = self.ram[self.pc+1]
+                # look in the identified register and find the value
+                val = register[self.reg]
+                # decriment the value i.e. memory address by 1
+                register[SP] -=1
+                # copy the value from the register into the memory
+                self.ram[register[SP]] = val
+                pc += 2 # because we had one argument
+            elif Command == POP:
+                # pop works the same way but in reverse
+                # look at the memory
+                sreg = memory[self.pc+1]
+                # memory of the register holding our SP
+                val = memory[register[SP]]
+
+                # copy that value into register that we are pointing at
+                memory[reg] = val
+
+                # incrememnt the stock pointer:
+                register[SP] += 1
+                pc += 2
+
+            else:
+                print(f"Unknown Instruction: {command}")
+                sys.exit(1)
