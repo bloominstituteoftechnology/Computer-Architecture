86d750a24c8aab375b92220881ed226f0ad1383b
Oluwajoba <oluwajobabello@gmail.com>
2020-03-25 16:21:11 +0100

    finish mvp for day 1 / cpu.py

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,13 +1,22 @@
 """CPU functionality."""
 
 import sys
+# `HLT` instruction handler
+HLT = 1
+# `LDI` instruction handler
+LDI = 130
+# `PRN` instruction handler
+PRN = 71
+
 
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0
 
     def load(self):
         """Load a program into memory."""
@@ -18,25 +27,32 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
+    def ram_read(self, address):
+        # should accept the address to read and return the value stored there.
+        return self.ram[address]
+
+    def ram_write(self, value, address):
+        # should accept a value to write, and the address to write it to
+        self.ram[address] = value
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +64,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -62,4 +78,29 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        # Implement the core of `CPU`'s `run()` method
+        self.load()
+
+        while True:
+            # needs to read mem address stores in register PC and store in IR - local variable
+            IR = self.ram_read(self.pc)
+            # read the bytes at `PC+1` and `PC+2` from RAM into variables `operand_a` and `operand_b` in case the instruction needs them
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+            # Then, depending on the value of the opcode, perform the actions needed for the instruction per the LS-8 spec. Maybe an `if-elif` cascade...? There are other options, too
+
+            if IR == HLT:
+                # In `run()` in your switch, exit the loop if a `HLT` instruction is encountered regardless of whether or not there are more lines of code in the LS-8 program you loaded
+                print("Exit")
+                break
+            elif IR == PRN:
+                # Add the `PRN` instruction
+                data = self.ram[self.pc + 1]
+                print(self.reg[data])
+                self.pc += 2
+            elif IR == LDI:
+                # Add the `LDI` instruction
+                self.reg[operand_a] = operand_b
+                self.pc += 3
+            else:
+                print("Error")
