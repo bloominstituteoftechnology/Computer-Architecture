0cb88d88a22b742a1484a853b35970fa16922c87
Uzias Rivera <uziasrivera@gmail.com>
2020-03-26 16:06:08 -0700

    completed project

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -23,11 +23,35 @@ class CPU:
         self.branchtable[0b01000111] = self.operand_PRN
         self.branchtable[0b10000010] = self.operand_LDI
         self.branchtable[0b10100010] = self.operand_MUL
+        self.branchtable[0b01000101] = self.operand_PUSH
+        self.branchtable[0b01000110] = self.operand_POP
+        self.branchtable[0b10100000] = self.operand_ADD
 
 
     def load(self):
         """Load a program into memory."""
-        self.read_params()
+        params = sys.argv
+        if len(params) != 2: # a file is being passed through
+            print("usage: file.py filename")
+            sys.exit(1)
+        if len(params)==2:   
+            try:
+                with open(params[1]) as f:
+                    address = 0
+                    for line in f:
+                        # print(line)
+                        comment_split = line.split("#")
+                        # Strip out whitespace
+                        num = comment_split[0].strip()
+                        # Ignore blank lines
+                        if num == '':
+                            continue
+                        val = int("0b"+num,2)
+                        self.ram_write(address, val)
+                        address += 1    
+            except FileNotFoundError:
+                print("File not found")
+                sys.exit(2)
 
 
     def alu(self, op, reg_a, reg_b):
@@ -67,30 +91,6 @@ class CPU:
 
     def ram_write(self, mar, mdr):
         self.ram[mar] = mdr
-
-    def read_params(self,):
-        params = sys.argv
-        if len(params) != 2: # a file is being passed through
-            print("usage: file.py filename")
-            sys.exit(1)
-        if len(params)==2:   
-            try:
-                with open(params[1]) as f:
-                    address = 0
-                    for line in f:
-                        # print(line)
-                        comment_split = line.split("#")
-                        # Strip out whitespace
-                        num = comment_split[0].strip()
-                        # Ignore blank lines
-                        if num == '':
-                            continue
-                        val = int("0b"+num,2)
-                        self.ram_write(address, val)
-                        address += 1    
-            except FileNotFoundError:
-                print("File not found")
-                sys.exit(2)
         
     def operand_LDI(self,):
         self.reg[self.ram_read(self.pc+1)] = self.ram_read(self.pc+2)
@@ -103,6 +103,24 @@ class CPU:
     def operand_MUL(self,):
         self.reg[self.ram_read(self.pc+1)]=(self.reg[self.ram_read(self.pc+1)] * self.reg[self.ram_read(self.pc+2)])
         self.pc +=3
+    def operand_PUSH(self,):
+        self.reg[self.sp] -= 1 # decrements as the stacks starts at the end
+        reg_num = self.ram[self.pc + 1]
+        register_value = self.reg[reg_num]
+        self.ram[self.reg[self.sp]] = register_value
+        self.pc += 2 # program counter
+    def operand_POP(self,):
+        value = self.ram[self.reg[self.sp]]
+        reg_num = self.ram[self.pc + 1]
+        self.reg[reg_num] = value
+        self.reg[self.sp] +=1
+        self.pc += 2
+    def operand_ADD(self,):
+        # print(self.pc, self.sp)
+        num_1 = self.reg[self.ram[self.pc+1]]
+        num_2 = self.reg[self.ram[self.pc+2]]
+        self.reg[self.ram[self.pc+1]] = num_1 + num_2
+        self.pc +=3
 
     def run(self):
         """Run the CPU.""" 
@@ -113,25 +131,24 @@ class CPU:
         MUL = 0b10100010
         PUSH = 0b01000101
         POP = 0b01000110
-
-        running = True
-        while running:
+        CALL = 0b01010000
+        RET = 0b00010001
+        ADD = 0b10100000
+        while not (self.ram_read(self.pc) is HLT):
             instruction = self.ram_read(self.pc)
-            if instruction == HLT:
-                running = False
-            elif instruction == PUSH:
-                self.reg[self.sp] -= 1 # decrements as the stacks starts at the end
+            if instruction == CALL:
+                # print("call")
+                return_address = self.pc + 2
+                self.reg[self.sp] -=1
+                self.ram[self.reg[self.sp]] = return_address
                 reg_num = self.ram[self.pc + 1]
-                register_value = self.reg[reg_num]
-                self.ram[self.reg[self.sp]] = register_value
-                self.pc += 2 # program counter
-            elif instruction == POP:
-                value = self.ram[self.reg[self.sp]]
-                reg_num = self.ram[self.pc + 1]
-                self.reg[reg_num] = value
-                self.reg[self.sp] +=1
-                self.pc += 2
+                self.pc = self.reg[reg_num]
+            elif instruction == RET:
+                # print("ret")
+                self.pc = self.ram[self.reg[self.sp]]
+                self.reg[self.sp] += 1
             else:
+                # print(instruction)
                 self.branchtable[instruction]()
 
 # pc = CPU()
