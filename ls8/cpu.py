8c6f0488032ae13894a5eec4c1c1ea0a99d18e0d
cmgospod <cmgospod@users.noreply.github.com>
2020-03-27 10:52:14 -0500

    MVP for sprint challenge

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -8,9 +8,10 @@ class CPU:
     def __init__(self, filename):
         """Construct a new CPU."""
         self.reg = [0] * 256
-        self.ram = [0] * 32
+        self.ram = [0] * 128
         self.filename = filename
-        self.stack_pointer = 0xF3
+        self.stack_pointer = 0xF4
+        self.flags = [0] * 8
     def load(self):
         """Load a program into memory."""
 
@@ -25,7 +26,7 @@ class CPU:
             self.ram[address] = eval(instruction)
             address += 1
 
-
+        # print(self.ram)
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
@@ -33,7 +34,14 @@ class CPU:
             self.reg[reg_a] += self.reg[reg_b]
         elif op == "MUL":
             self.reg[reg_a] *= self.reg[reg_b]
-
+        elif op == "CMP":
+            self.flags = [0] * 8
+            if self.reg[reg_a] < self.reg[reg_b]:
+                self.flags[5] = 1
+            elif self.reg[reg_a] == self.reg[reg_b]:
+                self.flags[6] = 1
+            elif self.reg[reg_a] > self.reg[reg_b]:
+                self.flags[7] = 1
         #elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
@@ -104,6 +112,26 @@ class CPU:
             elif instruction == 0b00010001:
                 address = self.reg[self.stack_pointer]
                 self.stack_pointer += 1
+            # CMP
+            elif instruction == 0b10100111:
+                self.alu("CMP", self.ram[address + 1], self.ram[address + 2])
+                address += 3
+            # JMP
+            elif instruction == 0b01010100:
+                address = self.reg[self.ram[address + 1]]
+            # JEQ
+            elif instruction == 0b01010101:
+                if self.flags[6] == 1:
+                    address = self.reg[self.ram[address + 1]]
+                else:
+                    address += 2
+            # JNE
+            elif instruction == 0b01010110:
+                if self.flags[6] != 1:
+                    address = self.reg[self.ram[address + 1]]
+                else:
+                    address += 2
+
             # HLT
             elif instruction == 0b00000001:
                 break
@@ -114,4 +142,6 @@ class CPU:
 
 
             else:
+                # print(address, 'address')
+                # print(instruction)
                 raise TypeError("That command has not been implemented yet")
