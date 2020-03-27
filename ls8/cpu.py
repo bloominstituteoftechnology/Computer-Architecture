8d5e8c05793073328d864e6c0bede9d447b64340
Oluwajoba <oluwajobabello@gmail.com>
2020-03-27 03:45:19 +0100

    add reg SP to CPU init / cpu.cy

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -24,6 +24,7 @@ class CPU:
         self.ram = [0] * 256
         self.reg = [0] * 8
         self.pc = 0
+        self.reg[SP] = 0XF4
 
     def load(self):
         """Load a program into memory."""
