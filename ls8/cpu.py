7925f0c0add6931a5fba149ad47fcee6f0a5c4ab
vdtyson <versilistyson@gmail.com>
2020-03-30 19:07:50 -0400

    MVP Day1

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,35 +1,58 @@
 """CPU functionality."""
 
 import sys
+fl_e = 0b100 # equal
+fl_l = 0b010 # less than
+fl_g = 0b001 # greater than
 
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.register = [0] * 8
+        self.pc = 0
+        self.fl = 0b00000001
 
-    def load(self):
+    def ram_read(self, location):
+        # print(location, self.ram[location])
+        return self.ram[location]
+
+    def ram_write(self, address, value):
+        self.ram[address] = value
+
+
+    def load(self, file_name):
         """Load a program into memory."""
-
         address = 0
+
+        # LDI = 0b10000010
+        # EIGHT = 0b00001000
+        # PRINT_NUM = 0b01000111
+        # HALT = 0b00000001
+
+        with open(file_name) as f:
+            lines = f.readlines()
+            lines = [line for line in lines if line.startswith('0') or line.startswith('1')]
+            program = [int(line[:8], 2) for line in lines]
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
         for instruction in program:
             self.ram[address] = instruction
+            # print('program statements',self.ram[address])
             address += 1
-
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
