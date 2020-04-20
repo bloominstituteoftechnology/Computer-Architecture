7ea5c08aa298090e2c716a5726d0ece8601f3027
ruwaidah <Ruwidah.riyadh@gmail.com>
2020-04-20 14:21:25 -0500

    MVP

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,26 @@
 
 import sys
 
+LDI = 130
+PRN = 71
+HLT = 1
+
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0
+
+    def ram_read(self, MAR):
+        value = self.ram[MAR]
+        return value
+
+    def ram_write(self, MAR, MDR):
+        self.ram[MAR] = MDR
 
     def load(self):
         """Load a program into memory."""
@@ -18,25 +32,24 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +61,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -62,4 +75,22 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        self.load()
+
+        while True:
+            IR = self.ram[self.pc]
+            if IR == LDI:
+                operand_a = self.ram_read(self.pc+1)
+                operand_b = self.ram_read(self.pc+2)
+                self.reg[0] = operand_b
+                self.pc += 2
+            elif IR == PRN:
+                print(self.reg[0])
+                self.pc += 1
+            elif IR == HLT:
+                break
+            self.pc += 1
+
+
+ddd = CPU()
+ddd.run()
