f2754887e4dc21ca91d9417148717933277c386d
ExtraJP2014 <extrajp2014@gmail.com>
2020-04-23 21:04:05 -0400

    done assignment 1

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,9 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.memory = [0]*256
+        self.register = [0]*8
+        self.pc = 0
 
     def load(self):
         """Load a program into memory."""
@@ -27,15 +29,15 @@ class CPU:
         ]
 
         for instruction in program:
-            self.ram[address] = instruction
+            self.memory[address] = instruction
             address += 1
 
 
-    def alu(self, op, reg_a, reg_b):
+    def alu(self, op, register_a, register_b):
         """ALU operations."""
 
         if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
+            self.register[register_a] += self.register[register_b]
         #elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
@@ -50,16 +52,46 @@ class CPU:
             self.pc,
             #self.fl,
             #self.ie,
-            self.ram_read(self.pc),
-            self.ram_read(self.pc + 1),
-            self.ram_read(self.pc + 2)
+            self.memory_read(self.pc),
+            self.memory_read(self.pc + 1),
+            self.memory_read(self.pc + 2)
         ), end='')
 
         for i in range(8):
-            print(" %02X" % self.reg[i], end='')
+            print(" %02X" % self.register[i], end='')
 
         print()
 
+    def memory_read(self, MAR):
+        return self.memory[MAR]
+
+    def memory_write(self, MDR, MAR):
+        self.memory[MAR] = MDR
+        return
+
     def run(self):
         """Run the CPU."""
-        pass
+        logic = True
+
+        LDI = 0b10000010
+        PRN = 0b01000111
+        HLT = 0b00000001
+
+        while logic:
+            IR = self.memory_read(self.pc)
+            operand_a = self.memory_read(self.pc + 1)
+            operand_b = self.memory_read(self.pc + 2)
+            if IR == LDI:
+                self.register[operand_a] = operand_b
+                self.pc += 3
+            elif IR == PRN:
+                print(self.register[operand_a])
+                self.pc += 2
+            elif IR == HLT:
+                self.pc += 1
+                break
+            else:
+                self.pc += 1
+                print(f"{IR} - command is not available")
+                sys.exit()
+                
