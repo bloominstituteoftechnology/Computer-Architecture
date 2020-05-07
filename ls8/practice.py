ab0ba29bc3176bed2952317c250815334ec8ea37
sweyome <sweyome1@gmail.com>
2020-05-06 21:29:02 -0400

    update

--- ls8/practice.py
+++ ls8/practice.py
@@ -1,24 +1,36 @@
 import sys
 
 
-PRINT_SENT = 1
-HALT       = 2
-PRINT_NUM  = 3
+PRINT_SENT     = 1
+HALT           = 2
+PRINT_NUM      = 3
+SAVE           = 4 # save a value to a register
+PRINT_REGISTER = 5 # print a  value from a register
+ADD            = 6 # regA += regB  add some values from regA to regB
 
-memory =[ # memory is an array of 0 and 1
+#! very slow comparing to a register
+memory =[ # memory is an array of 0 and 1 or RAM
     PRINT_SENT,
-    PRINT_NUM,
-    1,
-    PRINT_NUM,
-    12,
-    PRINT_SENT,
-    PRINT_NUM,
-    37,
+    SAVE,
+    65, #value
+    2, #register
+    SAVE,
+    20, #Value
+    3, # register
+    ADD,
+    2, #register
+    3, #register
+    PRINT_REGISTER,
+    2, #register
     HALT
 ]
 
-#  need to have a pointer to tell us which instruction 
+#! 8 bit register
+register = [0] * 8 # register memory are halt or built in the hard ware, fast and small
+# register represent numbers from 0 to 7
 
+
+#  need to have a pointer to tell us which instruction 
 pc = 0 # PC program counter starts from 0. 
 
 running = True
@@ -32,10 +44,27 @@ while running:# this is a processor
 
     elif command == HALT:
         running = False
-    elif command == PRINT_NUM:
-        num = memory[pc +1]
-        print(num)
-        pc +=2
+    # elif command == PRINT_NUM:
+    #     num = memory[pc +1]
+    #     print(num)
+    #     pc +=2
+    elif command == SAVE:
+        num = memory[pc + 1]
+        reg = memory[pc + 2]
+        register[reg] = num
+        pc +=3
+
+
+    elif command == PRINT_REGISTER:
+        reg = memory[pc + 1]
+        print(register[reg])
+        pc += 2
+
+    elif command == ADD:
+        reg_a = memory[pc + 1]
+        reg_b = memory[pc + 2]
+        register[reg_a] += register[reg_b]
+        pc += 3
     else:
         print(f"unknown instraction: {command}")
         sys.exit(1)
