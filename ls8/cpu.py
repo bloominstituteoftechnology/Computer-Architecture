96fbae365042f4159e0c92beb452d63eb830ad99
Chaz Rowe <chazr_88@hotmail.com>
2020-05-12 20:44:03 -0400

    finished day 1, working on day 2

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,34 +2,60 @@
 
 import sys
 
+LDI = 0
+HLT = 1
+PRN = 3
+
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
-        """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0
+        self.init_value = 0
 
-    def load(self):
+        
+    def load(self, program):
         """Load a program into memory."""
 
         address = 0
 
+        with open(sys.argv[1]) as f:
+            for line in f:
+                # print(line)
+                if line == '':
+                    continue
+                comment_split = line.split('#')
+                # print(comment_split) # [everything before #, everything after #]
+
+                num = comment_split[0].strip()
+
+                self.ram[address] = int(num)
+                address += 1
+
+
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+    def mult(self, a, b):
+        pass
 
+    def ram_read(self, MAR):
+        return self.ram[MAR]
+
+    def ram_write(self, MDR, MAR):
+        self.ram[MAR] = MDR
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -62,4 +88,32 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        HLT = 0b00000001
+        LDI = 0b10000010
+        PRN = 0b01000111
+        MUL = 0b10100010
+
+        running = True
+        ir = None
+
+        while running:
+            command = self.ram_read(self.pc)
+            reg_a = self.ram_read(self.pc + 1)
+            reg_b = self.ram_read(self.pc + 2)
+
+            if command == HLT:
+                running = False
+                self.pc += 1
+                sys.exit()
+            elif command == LDI:
+                self.reg[reg_a] = reg_b
+                self.pc += 3
+            elif command == PRN:
+                print(self.reg[reg_a])
+                self.pc += 2
+
+            else:
+                #If command is non recognizable
+                print(f"Unknown instruction")
+                running = False
+                #Lets Crash :(
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -5,7 +5,21 @@
 import sys
 from cpu import *
 
+# from examples import print8
+
+
+program = [
+    0b10000010, # LDI R0,8
+    0b00000000,
+    0b00001000,
+    0b01000111,# PRN R0
+    0b00000000,
+    0b00000001 # HLT
+
+]
+
+
 cpu = CPU()
 
-cpu.load()
+cpu.load(sys.argv[1])
 cpu.run()
