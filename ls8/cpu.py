46da4a66bbf826b1adf5991c58651899b8078275
Billy Gingrich <whgingrich@gmail.com>
2020-05-17 18:10:35 -0400

    day 1 MVP

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,12 +7,42 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        # PC
+        self.pc = 0
+        # 8 Registers
+        self.reg = [0] * 8
+        # 256 RAM
+        self.ram = [0] * 256
+        # Internal Register
+        # self.ir = 0
+        # # Stack Pointer
+        # self.sp = 7
+
+    
+    # RAM Read
+    def ram_read(self, mar):
+        # Memory Access Register
+        return self.ram[mar]
+
+    # RAM Write
+    def ram_write(self, mdr, mar):
+        # Memory Data Register
+        self.ram[mar] = mdr
 
     def load(self):
         """Load a program into memory."""
 
         address = 0
+        # with open(prog) as program:
+        #     for instruction in program:
+        #         instruction_split = instruction.split('#')
+        #         instruction_stripped = instruction_split[0].strip()
+
+        #         if instruction_stripped == '':
+        #             continue
+        #         instruction_num = int(instruction_stripped, 2)
+        #         self.ram_write(instruction_num, address)
+        #         address += 1
 
         # For now, we've just hardcoded a program:
 
@@ -37,6 +67,30 @@ class CPU:
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         #elif op == "SUB": etc
+        # if op == 'OR':
+        #     if reg_a == 1 and reg_b == 0:
+        #         return True
+        #     elif reg_a == 0 and reg_b == 1:
+        #         return True
+        #     elif reg_a == 1 and reg_b == 1:
+        #         return True
+        #     else:
+        #         return False
+        
+        # if op == 'XOR':
+        #     if reg_a == 1 and reg_b == 0:
+        #         return True
+        #     if reg_a == 0 and reg_b == 1:
+        #         return True
+        #     else:
+        #         return False
+
+        # if op == 'NOR':
+        #     if reg_a == 0 and reg_b == 0:
+        #         return True
+        #     else:
+        #         return False
+
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -62,4 +116,30 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        HLT = 0b00000001
+        LDI = 0b10000010
+        PRN = 0b01000111
+        MUL = 0b10100010
+        running = True
+
+        while running:
+            instruction = self.ram_read(self.pc)
+            reg_a = self.ram_read(self.pc + 1)
+            reg_b = self.ram_read(self.pc + 2)
+            if instruction == HLT:
+                running = False
+                self.pc += 1
+                sys.exit(0)
+            elif instruction == LDI:
+                self.reg[reg_a] = reg_b
+                self.pc += 3
+            elif instruction == PRN:
+                print(self.reg[reg_a])
+                self.pc += 2
+            elif instruction == MUL:
+                print(self.reg[reg_a] * self.reg[reg_b])
+                self.pc += 3
+            else:
+                print(f'This instruction is not valid: {hex(instruction)}')
+                running = False
+                sys.exit(1)            
