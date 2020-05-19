38ad2ec6b6cc6d8ca25cb7a9046a94f569574709
Dorothy Gasque <dggasque@gmail.com>
2020-05-19 11:03:34 -0700

    update cpu.py and ls8.py

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,35 +1,51 @@
 """CPU functionality."""
 
 import sys
+import re
 
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        # Initialize Register
+        self.reg = [0] * 8
 
-    def load(self):
+        # Initialize Memory
+        self.ram = [0] * 256
+
+        # Initialize internal registers
+        self.pc = 0
+        self.ir = 0
+        self.mar = 0
+        self.mdr = 0
+        self.fl = 0
+
+        # Initialize operation values
+        self.operand_a = 0
+        self.operand_b = 0
+
+        # Initialize halt as false. CPU does not start halted
+
+        self.halt = False
+
+        # Initialize branch_table
+        self.branch_table = {}
+        self.branch_table[0b10000010] = self.LDI
+        self.branch_table[0b01000111] = self.PRN
+        self.branch_table[0b00000001] = self.HLT
+        self.branch_table[0b10100010] = self.MUL
+
+
+    def load(self, program):
         """Load a program into memory."""
 
         address = 0
 
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
 
         for instruction in program:
-            self.ram[address] = instruction
+            self.ram_write(address, instruction)
             address += 1
-
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -60,6 +76,58 @@ class CPU:
 
         print()
 
+    def ram_read(self, address):
+        """
+        Function that reads from memory. Takes in the address to read.
+        Returns the value stored at that address in the RAM.
+        """
+        self.mdr = self.ram[address]
+
+        return self.mdr
+
+    def ram_write(self, address, value):
+        """
+        Function that writes to memory. Takes in the value to write,
+        the address to write to. Saves value at the given address.
+        """
+
+        self.ram[address] = value
+        self.mdr = self.ram[address]
+
     def run(self):
         """Run the CPU."""
-        pass
+
+        while not self.halt:         
+            self.mar = self.pc
+            self.ir = self.ram_read(self.mar)
+            self.operand_a = self.ram_read(self.pc+1)
+            self.operand_b = self.ram_read(self.pc+2)
+
+            self.branch_table[self.ir]()
+
+    def LDI(self):
+        """
+        Set register to this value
+        """
+        self.reg[self.operand_a] = self.operand_b
+        self.pc += 3
+
+    def PRN(self):
+        """
+        Prints numeric value stored at register address
+        """
+        print(self.reg[self.operand_a])
+        self.pc += 2
+
+    def HLT(self):
+        """
+        Sets halt value to true
+        """
+        self.halt = True
+    
+    def MUL(self):
+        """
+        Multiplies two numbers together.
+        """
+        self.reg[self.operand_a] *= self.reg[self.operand_b]
+        self.pc += 3
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -5,7 +5,20 @@
 import sys
 from cpu import *
 
+
+file = open(sys.argv[1], "r")
+
+program = []
+
+for line in file:
+    line = line.split(' ')[0]
+    line = line.split('#')[0]
+    if len(line) >= 8:
+        program.append(int(line, 2))
+
+file.close()
+
 cpu = CPU()
 
-cpu.load()
+cpu.load(program)
 cpu.run()
