c72c6e55336af54ca1e3199b799bc97bbd5016c3
Cody Holman <holman.cody@gmail.com>
2020-06-16 16:36:34 -0500

    still need load for MVP2

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,27 +7,24 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0
+        self.LDI = 0b10000010
+        self.PRN = 0b01000111
+        self.HLT = 0b00000001
+        self.MUL = 0b10100010
 
-    def load(self):
+    def load(self, program):
         """Load a program into memory."""
 
         address = 0
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
         for instruction in program:
             self.ram[address] = instruction
+            # print(f"i am an instruction: {self.ram[address]}")
             address += 1
 
 
@@ -36,9 +33,25 @@ class CPU:
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        #elif op == "SUB": etc\
+        elif op == "MUL":
+            #accept instruction
+            #write to reg_a
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
+
+    def ram_read(self, address):
+        # accept address
+        # return it's value
+        return self.ram[address]
+
+    def ram_write(self, value, address):
+        # take a value
+        # write to address
+        # no return
+        self.ram[address] = value
+
 
     def trace(self):
         """
@@ -59,7 +72,49 @@ class CPU:
             print(" %02X" % self.reg[i], end='')
 
         print()
+    
+
+
 
     def run(self):
         """Run the CPU."""
-        pass
+        
+        running = True
+        while running:
+            ir = self.ram[self.pc]
+
+            if ir == self.LDI:
+                self.ldi()
+
+            elif ir == self.PRN:
+                self.prn()
+
+            elif ir == self.HLT:
+                running = self.hlt()
+            elif ir == self.MUL:
+                running = self.mult()
+
+
+    def ldi(self):
+        address = self.ram[self.pc + 1]
+        value = self.ram[self.pc + 2]
+        self.reg[address] = value
+        self.pc += 3
+
+    def prn(self):
+        address = self.ram[self.pc + 1]
+        value = self.reg[address]
+        print(value)
+        self.pc += 2
+
+    def hlt(self):
+        self.pc += 1
+        return False
+
+    def mult(self):
+        # a = self.ram[self.pc + 1]
+        # b = self.ram[self.pc + 2]
+        self.alu("MUL", 0, 1)
+        print(f'RAM: {self.ram}')
+        print(f'Reg: {self.reg}')
+        self.pc += 3
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -5,7 +5,22 @@
 import sys
 from cpu import *
 
+mult = [
+    0b10000010, # LDI R0,8
+    0b00000000,
+    0b00001000,
+    0b10000010, # LDI R1,9
+    0b00000001,
+    0b00001001,
+    0b10100010, # MUL R0,R1
+    0b00000000,
+    0b00000001,
+    0b01000111, # PRN R0
+    0b00000000,
+    0b00000001# HLT
+]
+
 cpu = CPU()
 
-cpu.load()
+cpu.load(mult)
 cpu.run()
