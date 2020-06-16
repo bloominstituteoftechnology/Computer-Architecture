48d4283520ba75071165b434bb1032566e429efc
jarrodskahill <jarrodskahill@gmail.com>
2020-06-16 15:57:13 -0700

    finished day 1

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,14 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
-        """Construct a new CPU."""
-        pass
+        self.pc = 0
+        self.reg = [0] * 8
+        self.ram = [0] * 256
 
     def load(self):
         """Load a program into memory."""
@@ -18,25 +20,31 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
+    def ram_read(self, mar):
+
+        return self.ram[mar]
+
+    def ram_write(self, mar, mdr):
+        self.ram[mar] = mdr
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +56,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -62,4 +70,30 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        HLT = 0b00000001
+        LDI = 0b10000010
+        PRN = 0b01000111
+        MUL = 0b10100010
+        running = True
+
+        while running:
+            instruction = self.ram_read(self.pc)
+            reg_a = self.ram_read(self.pc + 1)
+            reg_b = self.ram_read(self.pc + 2)
+            if instruction == HLT:
+                running = False
+                self.pc += 1
+                sys.exit()
+            elif instruction == MUL:
+                print(self.reg[reg_a] * self.reg[reg_b])
+                self.pc += 3
+            elif instruction == PRN:
+                print(self.reg[reg_a])
+                self.pc += 2
+            elif instruction == LDI:
+                self.reg[reg_a] = reg_b
+                self.pc += 3
+            else:
+                print(f'this instruction is not valid: {hex(instruction)}')
+                running = False
+                sys.exit()
