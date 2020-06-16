b5e51a924c57d14735a1433d54a7ae57ef399f84
ThisIsJorgeLima <ThisIsJorgeLima@users.noreply.github.com>
2020-06-16 09:49:56 -0400

    MVP complete

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,36 @@
 
 import sys
 
+
+# Step 1: Add the constructor to cpu.py
+# x = [0] * 25  # x is a list of 25 zeroes
+# 256 bytes in total
+
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        # RAM
+        self.ram = [0] * 256
+        # Our registers
+        self.registers = [0] * 8  # 8 general-purpose registers, like variables, R0, R1, R2 .. R7
+        # internal registers
+        self.pc = 0  # Program Counter, index of the current instruction
+
+
+# Step 2: Add RAM functions
+# ram_read() should accept the address to read and return the value stored there.
+
+# ram_write() should accept a value to write, and the address to write it to.
+
+
+    def ram_read(self, address):
+        return self.ram[address]
+
+    def ram_write(self, value, address):
+        self.ram[address] = value
 
     def load(self):
         """Load a program into memory."""
@@ -18,25 +42,24 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +71,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -62,4 +85,42 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+
+
+# Step 3: Implement the core of CPU's run() method
+# Step 4: Implement the HLT instruction handler
+# Step 5: Add the LDI instruction
+# Step 6: Add the PRN instruction
+# Step 7: Un-hardcode the machine code
+# Step 8: Implement a Multiply and Print the Result
+# Step 9: Beautify your run() loop
+# Step 10: Implement System Stack
+# Step 11: Implement Subroutine Calls
+
+        HLT = 0b00000001  # Instruction handler
+        LDI = 0b10000010  # instruction
+        PRN = 0b01000111  # PRN instruction
+        running = True
+
+        self.ir = self.ram_read(self.pc)
+        reg_num1 = self.ram_read(self.pc + 1)
+        reg_num2 = self.ram_read(self.pc + 2)
+
+        while running:
+            # Lets set our value of our register
+            if self.ir == LDI:
+                self.reg[reg_num1] = reg_num2
+                self.pc += 3
+            # print
+            elif self.ir == PRN:
+                print(self.reg[reg_num1])  # print our stored value
+                self.pc += 2
+                # Halt our CPU and exit
+            elif self.ir == HLT:
+                running = False
+                self.pc += 1
+
+            else:
+                print(f"Unknown instruction {ir} at address {pc}")
+
+                sys.exit(1)
---
+++ notebooks/practice.py
@@ -0,0 +1,31 @@
+"""
+
+
+Print out all of the strings in the following array in alphabetical order, each on a separate line.
+['Waltz', 'Tango', 'Viennese Waltz', 'Foxtrot', 'Cha Cha', 'Samba', 'Rumba', 'Paso Doble', 'Jive']
+The expected output is:
+'Cha Cha'
+'Foxtrot'
+'Jive'
+'Paso Doble'
+'Rumba'
+'Samba'
+'Tango'
+'Viennese Waltz'
+'Waltz'
+You may use whatever programming language you'd like.
+Verbalize your thought process as much as possible before writing any code. Run through the UPER problem solving framework while going through your thought process.
+
+"""
+
+
+line = ['Waltz', 'Tango', 'Viennese Waltz', 'Foxtrot',
+        'Cha Cha', 'Samba', 'Rumba', 'Paso Doble', 'Jive']
+
+line.sort()  # (n log(n))
+# Python timsort
+# use the built in function
+# always (n log(n))
+# Bubble Sort
+for i in line:
+    print(i)
