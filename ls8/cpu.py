dd729e3a877b84d493e855f1188dd9d1e6955a6e
Chad Rutherford <chad_rutherford@outlook.com>
2020-06-16 16:46:25 -0400

    Added 'MUL' to the ALU method

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -45,6 +45,8 @@ class CPU:
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
         #elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
