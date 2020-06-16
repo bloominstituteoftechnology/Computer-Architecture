b35c834a2cfb8c541e057dbc3d0f5089ea6aef2e
Chad Rutherford <chad_rutherford@outlook.com>
2020-06-16 15:00:58 -0400

    Finished the run method

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -67,8 +67,22 @@ class CPU:
         print()
 
     def run(self):
-        """Run the CPU."""
-        pass
+        while self.running:
+            ir = self.ram_read(self.pc)
+            if ir == self.LDI:
+                reg_num = self.ram_read(self.pc + 1)
+                value = self.ram_read(self.pc + 2)
+                self.ram_write(reg_num, value)
+                self.pc += 3
+            elif ir == self.HLT:
+                self.running = False
+            elif ir == self.PRN:
+                reg_num = self.ram[self.pc + 1]
+                print(self.ram[reg_num])
+                self.pc += 2
+            else:
+                print(f'Unknown instruction {ir} at address {self.pc}')
+                sys.exit(1)
 
     def ram_read(self, address):
         return self.ram[address]
