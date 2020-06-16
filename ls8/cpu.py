956cacaf025c25714afbdb2b20218c9cb7e7d499
Chad Rutherford <chad_rutherford@outlook.com>
2020-06-16 14:39:13 -0400

    Created CPU constructor

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -6,7 +6,9 @@ class CPU:
     """Main CPU class."""
 
     def __init__(self):
-        """Construct a new CPU."""
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0
         pass
 
     def load(self):
