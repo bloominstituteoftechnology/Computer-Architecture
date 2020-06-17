9686696653e59055da8a07408766fac096514169
Prem Puttegowda <puttegowdaprem@gmail.com>
2020-06-17 13:59:39 -0700

    completed push and pop

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,6 +2,7 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
@@ -11,13 +12,16 @@ class CPU:
         self.pc = 0
         self.ram = [0] * 256
         self.running = True
+        self.sp = 7
+        self.reg[self.sp] = 0xF4
         self.ir = {
-            0b10100010:'MUL',
-            0b00000001:'HLT',
-            0b10000010:'LDI',
-            0b01000111:'PRN'
-            }
-     
+            0b10100010: 'MUL',
+            0b00000001: 'HLT',
+            0b10000010: 'LDI',
+            0b01000111: 'PRN',
+            0b01000101: 'PUSH',
+            0b01000110: 'POP'
+        }
 
     def load(self, file_name):
         """Load a program into memory."""
@@ -25,17 +29,16 @@ class CPU:
         address = 0
         program = []
         with open(file_name) as f:
-            
-         
+
             for address, line in enumerate(f):
                 line = line.split("#")
                 try:
                     v = int(line[0], 2)
-                    
+
                 except ValueError:
                     continue
                 self.ram[address] = v
-       
+
         # For now, we've just hardcoded a program:
 
         # program = [
@@ -52,23 +55,22 @@ class CPU:
         #     self.ram[address] = instruction
         #     address += 1
 
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
-        
-        value = op >> 6 
-        value = value + 1
+
+        value = op >> 6
+        value = value + 0b00000001
         # print('value', value)
         op = self.ir[op]
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         elif op == 'MUL':
             print(self.reg[reg_a] * self.reg[reg_b])
             self.pc += value
         elif op == 'LDI':
             self.ram_write(reg_a, reg_b)
-            self.pc += value #3
+            self.pc += value  # 3
         elif op == 'HLT':
             self.running = False
             self.pc += value
@@ -76,6 +78,26 @@ class CPU:
         elif op == 'PRN':
             print(self.reg[reg_a])
             self.pc += value
+        elif op == 'PUSH':
+
+            self.reg[self.sp] -= 1
+
+            reg_value = self.reg[reg_a]
+
+            stack_address = self.reg[self.sp]
+            self.ram[stack_address] = reg_value
+            self.pc += value
+        elif op == 'POP':
+            
+            stack_address = self.reg[self.sp]
+
+            reg_value = self.ram_read(stack_address)
+            self.reg[self.sp] = self.reg[self.sp] + 1
+
+            self.reg[reg_a] = reg_value
+
+            self.pc += value
+
         else:
             raise Exception("Unsupported ALU operation")
             sys.exit(1)
@@ -88,8 +110,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -99,63 +121,30 @@ class CPU:
             print(" %02X" % self.reg[i], end='')
 
         print()
-    
+
     def ram_read(self, counter):
         return self.ram[counter]
         # counter += counter
-        
-      
-    
-    def ram_write(self,counter, value):
-        self.reg[counter] = value
 
+    def ram_write(self, counter, value):
+        self.reg[counter] = value
 
     def run(self):
         """Run the CPU."""
-        HLT = 0b00000001
-        # ir = {
-        #     'MUL':0b10100010,
-        #     'HLT':0b00000001,
-        #     'LDI':0b10000010,
-        #     'PRN':0b01000111
-        #     }
-        ir = {
-            0b10100010:'MUL',
-            0b00000001:'HLT',
-            0b10000010:'LDI',
-            0b01000111:'PRN'
-            }
-            
-     
-         # 0b10000010
-       
-      
+
         while self.running:
-            # ir = self.ram_read(pc)
-            
-            # if ir == 0b10000010:
-              
-            #     self.ram_write(self.ram_read(pc+1), self.ram_read(pc+2))
-            #     print("LDI, pc, pc+1", ir, self.ram_read(pc+1), self.ram_read(pc+2)) 
-            #     pc +=3  #3
-            # elif ir == HLT:
-            #     running = False
-                
-            #     pc = pc + 1
-                
-            #     sys.exit(1)
-            # elif ir == 0b01000111:
-            #     print(self.ram_read(pc+1))
-                
-            #     pc = pc + 2
-            # elif ir == 0b10100010:
-            #     print(self.ram_read(pc), self.ram_read(pc+1), self.ram_read(pc+2))
-               
-            #     self.alu(self.ram_read(pc), self.ram_read(pc+1), self.ram_read(pc+2))
-                
-            # else:
-            #     print(f'Unknown instruction {ir} at address {pc}')
-            #     sys.exit(1)
-          
-            self.alu(self.ram_read(self.pc), self.ram_read(self.pc+1), self.ram_read(self.pc+2))
-            
+
+            self.alu(self.ram_read(self.pc), self.ram_read(
+                self.pc+1), self.ram_read(self.pc+2))
+
+            # PUSH
+# PUSH register
+
+# # Push the value in the given register on the stack.
+
+# # Decrement the SP.
+# # Copy the value in the given register to the address pointed to by SP.
+# # Machine code:
+
+# # 01000101 00000rrr
+# 45 0r
