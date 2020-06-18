f1585b32abbf0ad03c22ad0cfc5268f1e95f4318
Mike-Xie <Mike-Xie@users.noreply.github.com>
2020-06-18 09:16:59 -0700

    finish day 1 mvp: LDI, PRN, HLT, run

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -9,6 +9,7 @@ class CPU:
         self.reg = [0] * 8 # 8 registers
         self.ram = [0] * 256 # memory of 256 bit
         self.program_counter = 0
+        self.running = False 
     def load(self):
         """Load a program into memory."""
 
@@ -46,10 +47,13 @@ class CPU:
 # MAR contains the address that is being read or written to. 
 # he MDR contains> the data that was read or the data to write.
     
-    def ram_read(self, MAR, MDR):
-        pass 
-    def ram_write(self, MAR, MDR):
-        pass 
+    # return address in RAM
+    def ram_read(self, MAR) -> str:
+        return self.ram[MAR]
+
+    # write to RAM, return nothing
+    def ram_write(self, MAR, MDR) -> None:
+        self.ram[MAR] = MDR
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -69,12 +73,12 @@ class CPU:
         """
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
-            self.pc,
+            self.program_counter,
             #self.fl,
             #self.ie,
-            self.ram_read(self.pc),
-            self.ram_read(self.pc + 1),
-            self.ram_read(self.pc + 2)
+            self.ram_read(self.program_counter),
+            self.ram_read(self.program_counter + 1),
+            self.ram_read(self.program_counter + 2)
         ), end='')
 
         for i in range(8):
@@ -84,8 +88,26 @@ class CPU:
 
     # execute the instructions stored in self.ram 
     def run(self):
-        running = True
+        self.running = True
 
-        while running:
+        while self.running:
 
-            if 
+            IR = self.ram_read(self.program_counter)
+
+            if IR == 0b10000010:
+                operand_a = self.ram_read(self.program_counter + 1)
+                operand_b = self.ram_read(self.program_counter + 2)
+                self.ram_write(operand_b, operand_a)
+                self.program_counter += 3
+            elif IR == 0b01000111:
+                operand_a = self.ram_read(self.program_counter + 1)
+                self.ram_write(operand_b, operand_a)
+                print(f'printing... {self.ram[operand_a]}')           
+                self.program_counter += 2
+
+            elif IR == 0b00000001:
+                self.running = False
+                print("Program Halted")
+            else:
+                print("Unknown Instruction")
+                self.running = False
