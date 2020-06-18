b3711058075e19bab52bc3441e15afef582fecf4
jarrodskahill <jarrodskahill@gmail.com>
2020-06-18 02:43:10 -0700

    stack and mult completed

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -10,6 +10,7 @@ class CPU:
         self.pc = 0
         self.reg = [0] * 8
         self.ram = [0] * 256
+        self.SP = 0xf3
 
     def load(self):
         """Load a program into memory."""
@@ -18,15 +19,25 @@ class CPU:
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010,  # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111,  # PRN R0
-            0b00000000,
-            0b00000001,  # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010,  # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111,  # PRN R0
+        #     0b00000000,
+        #     0b00000001,  # HLT
+        # ]
+
+        program = []
+
+        with open(sys.argv[1]) as lines:
+            for line in lines:
+                value = line.split('#')[0].strip()
+                if value == '':
+                    continue
+                v = int(value, 2)
+                program.append(v)
 
         for instruction in program:
             self.ram[address] = instruction
@@ -45,6 +56,8 @@ class CPU:
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         # elif op == "SUB": etc
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -68,12 +81,22 @@ class CPU:
 
         print()
 
+    def push(self, address=None):
+        if address is None:
+            self.reg[7] -= 1
+            self.ram_write(self.reg[7], self.reg[self.ram_read(self.pc+1)])
+        else:
+            self.reg[7] -= 1
+            self.ram_write(self.reg[7], address)
+
     def run(self):
         """Run the CPU."""
         HLT = 0b00000001
         LDI = 0b10000010
         PRN = 0b01000111
         MUL = 0b10100010
+        PUSH = 0b01000101
+        POP = 0b01000110
         running = True
 
         while running:
@@ -85,7 +108,7 @@ class CPU:
                 self.pc += 1
                 sys.exit()
             elif instruction == MUL:
-                print(self.reg[reg_a] * self.reg[reg_b])
+                self.alu('MUL', reg_a, reg_b)
                 self.pc += 3
             elif instruction == PRN:
                 print(self.reg[reg_a])
@@ -93,6 +116,14 @@ class CPU:
             elif instruction == LDI:
                 self.reg[reg_a] = reg_b
                 self.pc += 3
+            elif instruction == PUSH:
+                self.ram_write(self.SP, self.reg[reg_a])
+                self.pc += 2
+                self.SP -= 1
+            elif instruction == POP:
+                self.reg[reg_a] = self.ram_read(self.SP+1)
+                self.SP += 1
+                self.pc += 2
             else:
                 print(f'this instruction is not valid: {hex(instruction)}')
                 running = False
