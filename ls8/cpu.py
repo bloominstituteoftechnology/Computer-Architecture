4c93369737fe1cb499e7902a0ac65d7a95747b0c
Nicole white <you@example.com>
2020-06-18 11:12:20 -1000

    day 3

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -10,12 +10,17 @@ class CPU:
         self.reg = [0] * 8
         self.pc = 0
         self.running = False
+        self.sp = 7 #our stack pointer starts at the top of a 0-7 index
     
     def call_stack(self, func):
         branch_table = {
             0b10000010: self.LDI,
             0b01000111: self.PRN,
             0b10100010: self.MULT,
+            0b01100101: self.INC,
+            0b01100110: self.DEC,
+            0b01000101: self.PUSH,
+            0b01000110: self.POP,
             0b00000001: self.HLT
         }
         if func in branch_table:
@@ -42,6 +47,32 @@ class CPU:
     def MULT(self):
         self.alu('MULT', self.pc+1, self.pc+2)
         self.pc += 3
+
+    def DEC(self):
+        self.alu('DEC', self.pc+1, None)
+
+    def INC(self):
+        self.alu('INC', self.pc+1, None)
+    
+    def PUSH(self):
+        # print('push')
+        #decrement SP
+        self.sp -=1
+        #get next value
+        reg_num = self.ram[self.pc + 1]
+        value = self.reg[reg_num]
+        #store it
+        self.ram[self.sp] = value
+        self.pc += 2
+    
+    def POP(self):
+        # print('pop')
+        value = self.ram[self.sp]
+        #go to next in ram
+        self.reg[self.ram[self.pc + 1]] = value
+        #increment sp
+        self.sp +=1
+        self.pc += 2
 
     def ram_read(self, index):
         return self.ram[index]
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -7,5 +7,5 @@ from cpu import *
 
 cpu = CPU()
 
-cpu.load("examples/mult.ls8")
+cpu.load("examples/stack.ls8")
 cpu.run()
