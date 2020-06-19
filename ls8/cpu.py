28266d5dfe4db65506c31ca55dee39df26bf36f2
Mike-Xie <Mike-Xie@users.noreply.github.com>
2020-06-19 01:50:05 -0700

    pass mvp for day 4, finish type annotations & comments about binary shifts, etc.

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,96 +2,145 @@
 
 import sys
 
-    
+# OP CODES AND what regular number their binary translates to 
+LDI = 0b10000010 # 130 
+PRN = 0b01000111 # 71
+HLT = 0b00000001 # 1
+ADD = 0b10100000 # 160 
+MUL = 0b10100010 # 162
+PUSH = 0b01000101 # 69
+POP = 0b01000110 # 70
+CALL = 0b01010000 # 80
+RET = 0b00010001 # 17 
 
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
-        self.reg = [0] * 8 # 8 registers
-        self.ram = [0] * 256 # memory of 256 bit
-        self.program_counter = 0
-        self.running = False 
-        # class variable for branch table
-        # stack pointer starts at 7 
-        stack_pointer = 7 
-        instruction_branch_table = {
-            "LDI" : 0b10000010,
-            "PRN" : 0b01000111,
-            "HLT" : 0b00000001,
-            "MULT" : 0b10100010,
-            "PUSH" : 0b01000101,
-            "POP" : 0b01000110,
-            "CALL" : 0b01010000,
-            "RET" : 0b00010001,
+        """Construct a new CPU."""
+        self.SP = 7 # stack pointer
+        self.pc = 0
+        self.reg = [0] * 8
+        self.ram = [0] * 256
+        self.running = True
+        self.instructions = {
+            LDI: self.LDI,
+            PRN: self.PRN,
+            ADD: self.ADD,
+            MUL: self.MUL,
+            PUSH: self.PUSH,
+            POP: self.POP,
+            HLT: self.HLT,
+            CALL: self.CALL,
+            RET: self.RET
         }
-    def load(self, filename):
-        """ 
-        load a program into ram, use run to run it
+    """
+        ALL OF THESE FUNCTIONS TAKE 2 args because it makes the load and run 
+        functions work more smoothly although most do not actually use 2 args
+    """
+    # LOAD
+    def LDI(self, operand_a: int, operand_b: int) -> None:
+        self.reg[operand_a] = operand_b
 
-        """
-        counter = 0 
+    # PRINT THING IN A
+    def PRN(self, operand_a: int, operand_b: int) -> None:
+        print(f'printing... {self.reg[operand_a]}')
+    
+    # call ALU integer add
+    def ADD(self, operand_a: int, operand_b: int) -> int:
+        self.alu('ADD', operand_a, operand_b)
+
+    # call ALU integer multiply
+    def MUL(self, operand_a: int, operand_b: int) -> int:
+        self.alu('MUL', operand_a, operand_b)
+    
+    def PUSH(self, operand_a: int, operand_b: int) -> None:
+        # decrement self.SP (stack pointer)
+        self.reg[self.SP] -= 1
+        # copy value from register into ram address pointed by self.SP
+        address = self.reg[self.SP]
+        self.ram_write(self.reg[operand_a], address)
+
+    def POP(self, operand_a: int, operand_b: int) -> int:
+        address = self.reg[self.SP]
+        # copy value from the self.SP address into the given register
+        self.reg[operand_a] = self.ram_read(address)
+        # increment self.SP (stack pointer)
+        self.reg[self.SP] += 1
+
+    def CALL(self, operand_a: int, operand_b: int) -> None: 
+        # decrement SP
+        self.reg[self.SP] -= 1
+        # push that instruction onto the stack
+        self.ram_write(self.pc + 2, self.reg[self.SP])
+        # PC is set to the address stored in the given register
+        # We jump to that location in RAM
+        self.pc = self.reg[operand_a]
+
+    def RET(self, operand_a: int, operand_b: int) -> None:
+        # Pop the value from the top of the stack and store it in the PC.
+        address = self.reg[self.SP]
+        self.pc = self.ram_read(address)
+        # increment self.SP (stack pointer)
+        self.reg[self.SP] += 1
+
+    # stops program since while loop looks for a false 
+    def HLT(self, operand_a: int, operand_b: int) -> None:
+        self.running = False
+
+    def load(self, filename) -> None:
+        """Load a program into memory."""
+
+        address = 0
+
         with open(filename, 'r') as f:
             program = f.readlines()
             for line in program:
-                line = line.split('#') # ignore comments
+                line = line.split('#')
                 line = line[0].strip()
 
-                if line =='': # ignore newlines
+                if line == '':
                     continue
-                self.ram[counter] = int(line, 2)
-                counter += 1
-    
-    # load thing at b into register at a        
-    def LDI(self, a, b):
-        self.reg[a] = b 
-    # print contents of thing at a 
-    def PRN(self, a, b):
-        print(f'contents of reg at b {self.reg[a]}')
-    # halt program by set running to False
-    # since run while loop checks for this 
-    def HLT(self):
-        self.running = False
-    def ADD(self, a, b):
-        self.alu("ADD", a, b)
-    def MUL(self, a, b):
-        self.alu("MUL", a, b)
+                # convert to binary and save to ram
+                # silly contrived thing in our python simulator
+                self.ram[address] = int(line, 2)
+                address += 1
 
-# MAR contains the address that is being read or written to. 
-# he MDR contains> the data that was read or the data to write.
-    
-    # return address in RAM
-    def ram_read(self, MAR) -> str:
-        return self.ram[MAR]
-
-    # write to RAM, return nothing
-    def ram_write(self, MAR, MDR) -> None:
-        self.ram[MAR] = MDR
-
-    def alu(self, op, reg_a, reg_b):
+    # math operations on A and B, storing and returning from A
+    def alu(self, op, reg_a: int, reg_b: int) -> int:
         """ALU operations."""
-
         if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+            self.reg[reg_a] += self.reg[reg_b] 
         elif op == "MUL":
-            self.reg[reg_a] *= self.reg[reg_b]    
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
-    def trace(self):
+        return self.reg[reg_a]
+
+    def ram_read(self, address: int) -> int:
+        """Return an address in RAM."""
+
+        return self.ram[address]
+
+    def ram_write(self, value: int, address: int) -> None:
+        """Set an address in RAM to a certain value."""
+
+        self.ram[address] = value
+
+    def trace(self) -> None:
         """
         Handy function to print out the CPU state. You might want to call this
         from run() if you need help debugging.
         """
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
-            self.program_counter,
+            self.pc,
             #self.fl,
             #self.ie,
-            self.ram_read(self.program_counter),
-            self.ram_read(self.program_counter + 1),
-            self.ram_read(self.program_counter + 2)
+            self.ram_read(self.pc),
+            self.ram_read(self.pc + 1),
+            self.ram_read(self.pc + 2)
         ), end='')
 
         for i in range(8):
@@ -99,39 +148,27 @@ class CPU:
 
         print()
 
-    # hard coded program
-        #     # program = [
-        #     # From print8.ls8          
-        #     0b10000010,  # LDI R0,8         
-        #     0b00000000,  # NOP: Do nothing for this instruction.            
-        #     0b00001000,  # this is the number 8        
-        #     0b01000111,  # PRN R0          
-        #     0b00000000,  # NOP: Do nothing for this instruction.          
-        #     0b00000001,  # HLT         
-        # ]          
-
-    # execute the instructions stored in self.ram 
     def run(self):
-        self.running = True
+        """Run the CPU."""
 
-        while self.running:
-
-            IR = self.ram_read(self.program_counter)
-
-            if IR == 0b10000010: #LDI
-                operand_a = self.ram_read(self.program_counter + 1)
-                operand_b = self.ram_read(self.program_counter + 2)
-                self.ram_write(operand_b, operand_a)
-                self.program_counter += 3
-            elif IR == 0b01000111: #PRN
-                operand_a = self.ram_read(self.program_counter + 1)
-                self.ram_write(operand_b, operand_a)
-                print(f'printing... {self.ram[operand_a]}')           
-                self.program_counter += 2
-
-            elif IR == 0b00000001: #HLT
-                self.running = False
-                print("Program Halted")
+        while self.running is True:
+            IR = self.ram_read(self.pc)
+            """
+            Conveniently:
+                op codes under 63 have an instruction length of 0 + 1
+                op codes under 127 have an instruction length of 1 + 1
+                op codes under 255 have an instruction length of 2 + 1
+            """
+            instruction_length = (IR >> 6) + 1 
+            operand_a = self.ram_read(self.pc + 1) 
+            operand_b = self.ram_read(self.pc + 2)
+            # if the op code is in the instructions, execute from branch table
+            if IR in self.instructions:
+                self.instructions[IR](operand_a, operand_b)
             else:
-                print("Unknown Instruction")
-                self.running = False
+                print("Invalid instruction")
+            if not IR & 0b00010000: # increment program counter when not HALT which is the only bitwise AND that returns non zero
+                self.pc += instruction_length
+
+            # self.pc += instruction_length
+            # self.trace()
