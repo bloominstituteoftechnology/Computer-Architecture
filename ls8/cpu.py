a79ae765d675a753577c8b415c63868dd9c361e3
Brianna Keune <briannakeune@gmail.com>
2020-07-20 14:15:58 -0500

    added ram functions

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,11 @@
 
 import sys
 
-class CPU:
-    """Main CPU class."""
 
+class CPU:
     def __init__(self):
         self.registers = [0] * 8
-        self.memory = [0] * 256
+        self.ram = [0] * 256
 
     def load(self):
         """Load a program into memory."""
@@ -18,25 +17,24 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +46,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -63,3 +61,15 @@ class CPU:
     def run(self):
         """Run the CPU."""
         pass
+
+    def ram_read(self, MAR):
+        '''
+        Returns what is stored at the given address.
+        '''
+        return self.ram[MAR]
+
+    def ram_write(self, MDR, MAR):
+        '''
+        Writes the given value to the address given.
+        '''
+        self.ram[MAR] = MDR
