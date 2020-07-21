015006f6161d0d5092682de98d2a21e07a477c76
Hector <hectoremil21@hotmail.com>
2020-07-21 13:59:04 -0400

    Finish tweaking parser. Can now run print8.ls8 from command line

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -23,22 +23,22 @@ class CPU:
         return self.ram[addy]
 
 
-    def load(self):
+    def load(self, program):
         """Load a program into memory."""
 
         address = 0
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
         for instruction in program:
             self.ram[address] = instruction
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -12,12 +12,12 @@ existing_files = [file for file in listdir(examples) if isfile(join(examples, fi
 
 if len(sys.argv) < 2 or len(sys.argv) > 2 or not sys.argv[1].endswith(".ls8"):
     print("USAGE: py ls8.py FILE_NAME.ls8")
-    exit()
+    # exit()
 elif sys.argv[1] not in existing_files:
     print("Specified file not in /examples directory.")
     print("OPTIONS:")
     print(existing_files)
-    exit()
+    # exit()
 else:
 
     file = open(join(examples, sys.argv[1]), "r")
@@ -28,13 +28,20 @@ else:
     # For the rest of the lines:
     # - Use .strip() to get rid of special characters
     # - use .split() to separate everything after "#" and take out only index 0
-    code = [line.strip().split('#',1)[0] for line in file.readlines() if not line.startswith("#") and line.strip()]
+    # - use .strip() again to get rid of any left over spaces
+    # And after all is said and done, conver the left-over string into an int with base 2
+    code = [int(line.strip().split('#',1)[0].strip(), 2) for line in file.readlines() if not line.startswith("#") and line.strip()]
 
-    print(code)
-    for line in code:
-        print(line)
     file.close()
 
-    # cpu = CPU()
-    # cpu.load()
-    # cpu.run()
+    cpu = CPU()
+    cpu.load(code)
+    cpu.run()
+
+# file = open(join(examples, "print8.ls8"), "r")
+# code = [int(line.strip().split('#',1)[0].strip(), 2) for line in file.readlines() if not line.startswith("#") and line.strip()]
+# print(code)
+# file.close()
+# cpu = CPU()
+# cpu.load(code)
+# cpu.run()
