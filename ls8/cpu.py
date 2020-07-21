c9b17c1ead433afdadefd6008bc62a35822f8386
Aaron Cleveland <toscleveland@gmail.com>
2020-07-21 12:06:56 -0500

    day1

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,17 @@
 
 import sys
 
-class CPU:
-    """Main CPU class."""
+HTL = 0b00000001
+LDI = 0b10000010
+PRN = 0b01000111
 
+
+class CPU:
     def __init__(self):
-        """Construct a new CPU."""
-        pass
+        self.reg = [0] * 8
+        self.ram = [0] * 256
+        self.pc = 0
+        self.fl = 0
 
     def load(self):
         """Load a program into memory."""
@@ -18,25 +23,24 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +52,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -62,4 +66,64 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+
+        while True:
+            instruction = bin(self.ram_read(self.pc))
+
+            print('instruction: ', instruction[2:])
+
+            IR = self.ram_read(self.pc)
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+
+            if IR is HTL:
+                self.handle_halt()
+            elif IR is LDI:
+                self.handle_ldi(operand_a, operand_b)
+            elif IR is PRN:
+                self.handle_print(operand_a)
+
+            self.pc += 1
+
+    def handle_halt(self):
+        # cpu reset
+        for i in range(0, 6):
+            self.reg[i] = 0
+        self.reg[7], self.pc, self.fl, self.ram == 0xF4, 0, 0, 0
+
+        sys.exit(1)
+
+    def handle_print(self, index):
+        '''
+        print numeric value stored at given register
+        '''
+        print(self.reg[index])
+
+    def handle_ldi(self, register_index, value):
+        '''
+        set the value of a register to an integer
+        '''
+        if register_index < len(self.reg):
+            self.reg[register_index] = value
+        else:
+            raise IndexError
+
+    def ram_read(self, MAR):
+        '''
+        Returns what is stored at the given address.
+        '''
+        try:
+            if MAR < len(self.ram):
+                return self.ram[MAR]
+        except IndexError:
+            return f'address is out of range.'
+
+    def ram_write(self, MAR, MDR):
+        '''
+        Writes the given value to the address given.
+        '''
+        try:
+            if MAR < len(self.ram):
+                self.ram[MAR] = MDR
+        except IndexError:
+            return f'address is out of range.'
