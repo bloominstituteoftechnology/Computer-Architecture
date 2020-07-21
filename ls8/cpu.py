7733eb216d2530a7ea4e4bfd7b3d758d0773b447
Wyatt Harrell <wyattaharrell@gmail.com>
2020-07-21 16:16:04 -0400

    Day 1 MVP

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,10 +7,10 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        reg = [0] * 8
-        ram = [0] * 256
-        PC = 0        # Program Counter, address of the currently executing instruction
-        IR = None     # Instruction Register, contains a copy of the currently executing instruction
+        self.reg = [0] * 8
+        self.ram = [0] * 256
+        self.pc = 0        # Program Counter, address of the currently executing instruction
+        self.IR = None     # Instruction Register, contains a copy of the currently executing instruction
         # MAR = None  # Memory Address Register, holds the memory address we're reading or writing
         # MDR = None  # Memory Data Register, holds the value to write or the value just read
 
@@ -19,10 +19,42 @@ class CPU:
         # R7 is reserved as the stack pointer (SP)
 
     def ram_read(self, MAR):
-        return self.reg[MAR]
+        if MAR < len(self.ram):
+            return self.ram[MAR]
 
     def ram_write(self, MAR, MDR):
-        self.reg[MAR] = MDR
+        if MAR < len(self.ram):
+            self.ram[MAR] = MDR
+
+    def run(self):
+        """Run the CPU."""
+        HTL = 0b00000001
+        LDI = 0b10000010
+        PRN = 0b01000111
+
+        while True:
+            inst = bin(self.ram[self.pc])
+            IR = self.ram_read(self.pc)
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+
+            if IR is HTL:
+                self.hlt_inst()
+            elif IR is PRN:
+                self.prn_inst(operand_a)
+            elif IR is LDI:
+                self.ldi_inst(operand_a, operand_b)
+            self.pc += 1
+
+    def hlt_inst(self):
+        exit(1)
+
+    def ldi_inst(self, MAR, MDR):
+        if MAR < len(self.ram):
+            self.reg[MAR] = MDR
+
+    def prn_inst(self, reg):
+        print(self.reg[reg])
 
     def load(self):
         """Load a program into memory."""
@@ -74,8 +106,3 @@ class CPU:
             print(" %02X" % self.reg[i], end='')
 
         print()
-
-    def run(self):
-        """Run the CPU."""
-        pass
-        self.IR = self.PC
