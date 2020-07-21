3b460ef661684b2a02ea0772ac3f5f833fa89fde
Hector <hectoremil21@hotmail.com>
2020-07-21 14:25:13 -0400

    Implement branch table

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -10,11 +10,25 @@ class CPU:
         self.ram = [0] * 256
         self.reg = [0] * 8
         self.pc = 0
+        self.isRunning = True
+
+        # self.instruction_translate = {
+        #     0b00000001: "HLT",
+        #     0b10000010: "LDI",
+        #     0b01000111: "PRN",
+        #     0b10100010: "MUL"
+        # }
+        # self.instruction_set = {
+        #     "HLT": self.HLT,
+        #     "LDI": self.LDI,
+        #     "PRN": self.PRN,
+        #     "MUL": self.MUL
+        # }
         self.instruction_set = {
-            "HLT": 0b00000001,
-            "LDI": 0b10000010,
-            "PRN": 0b01000111,
-            "MUL": 0b10100010
+            0b00000001: self.HLT,
+            0b10000010: self.LDI,
+            0b01000111: self.PRN,
+            0b10100010: self.MUL
         }
 
     def ram_write(self, val, addy):
@@ -84,24 +98,24 @@ class CPU:
 
         running = True
 
-        while running:
+        while self.isRunning:
 
-            # Instruction Register
-            instruction = self.ram_read(self.pc)
-            if instruction is self.instruction_set["LDI"]:
-                reg_a = self.ram_read(self.pc+1)
-                int = self.ram_read(self.pc+2)
-                self.reg[reg_a] = int
-                self.pc += 3
-            elif instruction is self.instruction_set["MUL"]:
-                print("We multiplied")
-                reg_a = self.ram_read(self.pc+1)
-                reg_b = self.ram_read(self.pc+2)
-                self.alu("MUL", reg_a, reg_b)
-                self.pc += 3
-            elif instruction is self.instruction_set["PRN"]:
-                print(self.reg[self.ram_read(self.pc+1)])
-                self.pc += 2
-            elif instruction is self.instruction_set["HLT"]:
-                running = False
-            # self.pc += 1
+            # Grab the bytecode instruction out of memory
+            # Use the instruction to access or branch table
+            self.instruction_set[self.ram_read(self.pc)]()
+
+    def LDI(self):
+        reg_a = self.ram_read(self.pc + 1)
+        int = self.ram_read(self.pc + 2)
+        self.reg[reg_a] = int
+        self.pc += 3
+    def MUL(self):
+        reg_a = self.ram_read(self.pc + 1)
+        reg_b = self.ram_read(self.pc + 2)
+        self.alu("MUL", reg_a, reg_b)
+        self.pc += 3
+    def PRN(self):
+        print(self.reg[self.ram_read(self.pc + 1)])
+        self.pc += 2
+    def HLT(self):
+        self.isRunning = False
