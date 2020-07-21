b538dbbde27719f4e49431f4cea097d6923c4ff9
Brandon-Harris <brandonharris177@gmail.com>
2020-07-21 13:21:55 -0600

    machine unhardcoded

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -53,24 +53,28 @@ class CPU:
     def load(self, program = None):
         """Load a program into memory."""
 
-        address = 0
+        if len(sys.argv) < 2:
+            print("Please pass in a second filename: python3 in_and_out.py second_filename.py")
+            sys.exit()
 
-        # For now, we've just hardcoded a program:
+        try:
+            address = 0
+            with open(program) as file:
+                for line in file:
+                    split_line = line.split('#')[0]
+                    command = split_line.strip()
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+                    if command == '':
+                        continue
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+                    instruction = int(command, 2)
+                    self.ram[address] = instruction
 
+                    address += 1
+
+        except FileNotFoundError:
+            print(f'{sys.argv[0]}: {sys.argv[1]} file was not found')
+            sys.exit()
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -7,5 +7,7 @@ from cpu import *
 
 cpu = CPU()
 
-cpu.load()
+file_name = sys.argv[1]
+
+cpu.load(file_name)
 cpu.run()
