a8d2b56590faa803c3cc2401863dbfd8b2e600a2
Brianna Keune <briannakeune@gmail.com>
2020-07-21 11:09:38 -0500

    d1 mvp

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,11 +2,17 @@
 
 import sys
 
+HTL = 0b00000001
+LDI = 0b10000010
+PRN = 0b01000111
+
 
 class CPU:
     def __init__(self):
-        self.registers = [0] * 8
+        self.reg = [0] * 8
         self.ram = [0] * 256
+        self.pc = 0
+        self.fl = 0
 
     def load(self):
         """Load a program into memory."""
@@ -60,16 +66,64 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+
+        while True:
+            instruction = bin(self.ram_read(self.pc))
+
+            print('instruction: ', instruction[2:])
+
+            IR = self.ram_read(self.pc)
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+
+            if IR is HTL:
+                self.handle_halt()
+            elif IR is LDI:
+                self.handle_ldi(operand_a, operand_b)
+            elif IR is PRN:
+                self.handle_print(operand_a)
+
+            self.pc += 1
+
+    def handle_halt(self):
+        # cpu reset
+        for i in range(0, 6):
+            self.reg[i] = 0
+        self.reg[7], self.pc, self.fl, self.ram == 0xF4, 0, 0, 0
+
+        sys.exit(1)
+
+    def handle_print(self, index):
+        '''
+        print numeric value stored at given register
+        '''
+        print(self.reg[index])
+
+    def handle_ldi(self, register_index, value):
+        '''
+        set the value of a register to an integer
+        '''
+        if register_index < len(self.reg):
+            self.reg[register_index] = value
+        else:
+            raise IndexError
 
     def ram_read(self, MAR):
         '''
         Returns what is stored at the given address.
         '''
-        return self.ram[MAR]
+        try:
+            if MAR < len(self.ram):
+                return self.ram[MAR]
+        except IndexError:
+            return f'address is out of range.'
 
-    def ram_write(self, MDR, MAR):
+    def ram_write(self, MAR, MDR):
         '''
         Writes the given value to the address given.
         '''
-        self.ram[MAR] = MDR
+        try:
+            if MAR < len(self.ram):
+                self.ram[MAR] = MDR
+        except IndexError:
+            return f'address is out of range.'
