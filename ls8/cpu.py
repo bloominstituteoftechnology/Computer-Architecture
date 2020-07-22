f54cdda8d7377a55285e410cc39e104f8b09c8cd
Ignacio San Martin <ignaciosm@gmail.com>
2020-07-22 15:32:21 -0500

    day 2

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -19,26 +19,25 @@ class CPU:
     def ram_write(self, MAR, MDR):
         self.ram[MAR] = MDR
 
-    def load(self):
-        """Load a program into memory."""
+    def load(self, program):
+        try:
+            address = 0
+            with open(program) as file:
+                for line in file:
+                    split_line = line.split('#')[0]
+                    command = split_line.strip()
 
-        address = 0
+                    if command == '':
+                        continue
 
-        # For now, we've just hardcoded a program:
+                    instruction = int(command, 2)
+                    self.ram_write(address, instruction)
 
-        # program = [
-        #     # From print8.ls8
-        #     0b10000010,  # LDI R0,8
-        #     0b00000000,
-        #     0b00001000,
-        #     0b01000111,  # PRN R0
-        #     0b00000000,
-        #     0b00000001,  # HLT
-        # ]
+                    address += 1
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        except FileNotFoundError:
+            print(f'{sys.argv[0]}: {sys.argv[1]} file was not found')
+            sys.exit()
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -46,6 +45,10 @@ class CPU:
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         # elif op == "SUB": etc
+
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
+
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -80,6 +83,7 @@ class CPU:
         LDI = 0b10000010
         PRN = 0b01000111
         HLT = 0b00000001
+        MUL = 0b10100010
 
         running = True
         while running:
@@ -97,3 +101,7 @@ class CPU:
 
             if IR == HLT:
                 running = False
+
+            if IR == MUL:
+                self.alu("MUL", operand_a, operand_b)
+                self.PC += 3
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -7,5 +7,7 @@ from cpu import *
 
 cpu = CPU()
 
+# cpu.load()
 cpu.load(sys.argv[1])
-cpu.run()
+# print('sys.argv[1]', sys.argv[1])
+# cpu.run()
