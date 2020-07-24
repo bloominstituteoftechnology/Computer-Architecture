e07e4c4cab0804b48ad3c2347b6d5f4a59556e5a
Shawn Gee <shawngee@gmail.com>
2020-07-24 11:52:40 -0400

    Sprint Challenge

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -41,14 +41,21 @@ XOR  = 0b10101011
 
 # Bit Masks
 
-ALU_MASK      = 0b00100000
-PC_MASK       = 0b00010000
+ALU_MASK = 0b00100000
+PC_MASK  = 0b00010000
+
+L_MASK   = 0b00000100
+G_MASK   = 0b00000010
+E_MASK   = 0b00000001
 
 # Offsets
 
 OPERANDS_OFFSET = 6
 ALU_OFFSET = 5
 PC_OFFSET = 4
+
+L_OFFSET = 2
+G_OFFSET = 1
 
 class CPU:
     """Main CPU class."""
@@ -58,6 +65,7 @@ class CPU:
         self.ram = [0] * 256
         self.reg = [0] * 8
         self.pc = 0
+        self.fl = 0
         self.stack_pointer = 0xF7
         self.configure_dispatch_table()
 
@@ -66,11 +74,17 @@ class CPU:
 
         self.dispatch_table[CALL] = self.call
         self.dispatch_table[HLT] = self.hlt
+        self.dispatch_table[JEQ] = self.jeq
+        self.dispatch_table[JMP] = self.jmp
+        self.dispatch_table[JNE] = self.jne
+        self.dispatch_table[LD] = self.ld
         self.dispatch_table[LDI] = self.ldi
         self.dispatch_table[POP] = self.pop
+        self.dispatch_table[PRA] = self.pra
         self.dispatch_table[PRN] = self.prn
         self.dispatch_table[PUSH] = self.push
         self.dispatch_table[RET] = self.ret
+        self.dispatch_table[ST] = self.st
 
     @property
     def stack_pointer(self):
@@ -80,6 +94,22 @@ class CPU:
     def stack_pointer(self, value):
         value &= 0xff
         self.reg[7] = value
+
+    @property
+    def interrupt_status(self):
+        return self.reg[6]
+
+    @interrupt_status.setter
+    def interrupt_status(self, value):
+        self.reg[6] = value
+
+    @property
+    def interrupt_mask(self):
+        return self.reg[5]
+
+    @interrupt_mask.setter
+    def interrupt_mask(self, value):
+        self.reg[5] = value
         
     def ram_read(self, address) -> int:
         return self.ram[address]
@@ -110,7 +140,12 @@ class CPU:
         elif op == AND:
             self.reg[a] = self.reg[a] & self.reg[b]
         elif op == CMP:
-            pass # TODO: requires setting flags
+            if self.reg[a] < self.reg[b]:
+                self.fl = L_MASK
+            elif self.reg[a] > self.reg[b]:
+                self.fl = G_MASK
+            else:
+                self.fl = E_MASK
         elif op == DEC:
             self.reg[a] -= 1
         elif op == DIV:
@@ -170,12 +205,33 @@ class CPU:
     def hlt(self):
         sys.exit()
 
+    def jeq(self, a):
+        if self.fl & E_MASK:
+            self.pc = self.reg[a]
+        else:
+            self.pc += 2
+
+    def jmp(self, a):
+        self.pc = self.reg[a]
+
+    def jne(self, a):
+        if not self.fl & E_MASK:
+            self.pc = self.reg[a]
+        else:
+            self.pc += 2
+
+    def ld(self, a, b):
+        self.reg[a] = self.ram_read(self.reg[b])
+
     def ldi(self, a, value):
         self.reg[a] = value
 
     def pop(self, a):
         self.reg[a] = self.ram_read(self.stack_pointer)
         self.stack_pointer += 1
+
+    def pra(self, a):
+        print(chr(self.reg[a]))
 
     def prn(self, a):
         print(self.reg[a])
@@ -188,6 +244,9 @@ class CPU:
         # Pop address from stack and set pc to that address
         self.pc = self.ram_read(self.stack_pointer)
         self.stack_pointer += 1
+
+    def st(self, a, b):
+        self.ram_write(self.reg[a], self.reg[b])
 
     def run(self):
         """Run the CPU."""
