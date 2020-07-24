18b8db2e3b002cbc3dc8a9e17e95ec95501dd79b
Henry <henrybsms@outlook.com>
2020-07-23 21:13:11 -0400

    day3

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,55 +2,89 @@
 
 import sys
 
+# Instructions
+LDI = 0b10000010 
+PRN = 0b01000111    # Print
+HLT = 0b00000001    # Halt
+MUL = 0b10100010    # Multiply
+ADD = 0b10100000    # Addition
+PUSH = 0b01000101   # Push in stack
+POP = 0b01000110    # Pop from stack
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        self.ram = [0] * 256    # stores the program on load()
-        self.reg = [0] * 8      # 8 registers
-        self.pc = 0             # current address
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.reg[7] = 0xF4
+        self.pc = 0
+        self.running = True
 
+    def ram_read(self, address):
+        return self.ram[address]
+
+    def ram_write(self, address, value):
+        self.ram[address] = value
+    
+    def hlt(self):
+        self.running = False
+        self.pc += 1
+    
+    def ldi(self, reg_num, value):
+        self.reg[reg_num] = value
+        self.pc += 3
+
+    def prn(self, reg_num):
+        print(self.reg[reg_num])
+        self.pc += 2
+    
     def load(self):
         """Load a program into memory."""
+        # # hardcoded:
+        # address = 0
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
 
-        # For now, we've just hardcoded a program:
+        # load an .ls8 file given the filename passed in as an argument
+        file_name = sys.argv[1]
+        try:
+            address = 0
+            with open(file_name) as file:
+                for line in file:
+                    split_line = line.split('#')[0]
+                    command = split_line.strip()
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+                    if command == '':
+                        continue
 
-        address = 0
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+                    instruction = int(command, 2)
+                    self.ram[address] = instruction
+                    address += 1
+        except FileNotFoundError:
+            print(f'{sys.argv[0]}: {sys.argv[1]} file was not found')
+            sys.exit()
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
-
-    def ldi(self, reg, val):
-        reg = reg & 0b00000111 # bitwise AND to prevent out-of-index
-        val = val & 0b11111111 # bitwise AND to limit values
-        self.reg[reg] = val
-
-    def ram_read(self, addr):
-        return self.ram[addr]
-
-    def ram_write(self, val, addr):
-        self.ram[addr] = val
 
     def trace(self):
         """
@@ -74,24 +108,58 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        self.pc = 0
-        ir = 0 # instruction register
+        # load the instructions file
+        self.load()
+        while self.running:
+            instruction_register = self.ram[self.pc]
+            reg_a = self.ram[self.pc+1]
+            reg_b = self.ram[self.pc+2]
 
-        while True: # use HLT to escape loop
-            ir = self.ram_read(self.pc)
+            if instruction_register == HLT:
+                self.hlt()
+            
+            elif instruction_register == LDI:
+                self.ldi(reg_a, reg_b)
+            
+            elif instruction_register == PRN:
+                self.prn(reg_a)
+            
+            elif instruction_register == MUL:
+                self.alu("MUL", reg_a, reg_b)
+                self.pc += 3
+            
+            elif instruction_register == PUSH:
+                # decrement stack pointer
+                self.reg[7] -= 1
 
-            if ir == 0b00000001:    # halt command
-                break
-            elif ir == 0b10000010:  # LDI: set register value
-                mem_addr_reg = self.ram_read(self.pc + 1)
-                mem_data_reg = self.ram_read(self.pc + 2)
-                self.ldi(mem_addr_reg, mem_data_reg)
-                self.pc += 3 # increment pc counter
-            elif ir == 0b01000111:  # PRN: print from register
-                mem_addr_reg = self.ram_read(self.pc + 1)
-                mem_addr_reg = mem_addr_reg & 0b00000111 # OoB limiter
-                print(self.reg[mem_addr_reg])
-                self.pc += 2 # increment pc counter
+                # get the value from a given register
+                reg_a = self.ram[self.pc+1]
+                value = self.reg[reg_a]
+
+                # put it on the stack pointer address
+                sp = self.reg[7]
+                self.ram[sp] = value
+
+                # increment pc
+                self.pc += 2
+
+            elif instruction_register == POP:
+                # get the tack pointer
+                sp = self.reg[7]
+
+                # get register number to put value in
+                reg_a = self.ram[self.pc+1]
+
+                # use stack pointer to get the value
+                value = self.ram[sp]
+                # put the value into a given register
+                self.reg[reg_a] = value
+
+                # increment stack pointer
+                self.reg[7] += 1
+                # increment program counter
+                self.pc += 2
+            
             else:
-                print(f'Unsupported opcode: {ir} at address: {self.pc}')
-                break
+                print(f"Instruction number {self.pc} not recognized!")
+                self.pc += 1
