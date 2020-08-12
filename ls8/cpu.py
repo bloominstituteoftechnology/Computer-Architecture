fed4dc4103dababf1684b91544cdad10bfccf3cd
ajohnson1031 <aaron.johnson1031@gmail.com>
2020-08-12 16:32:59 -0400

    refactored day 1 exercises, completed day 2 exercises

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,15 +2,24 @@
 
 import sys
 
+TGREEN =  '\033[32m'
+TRED = '\033[31m'
+TYELLOW = '\033[33m'
+ENDC = '\033[m'
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        self.ram = [0] * 8
+        self.ram = [0] * 256
+        self.reg = [0] * 8
         self.pc = 0
         self.HLT = '1'
-        self.LDI = '10000010'        
+        self.LDI = '10000010'
+        self.PRN = '1000111'
+        self.MULT = '10100010'
+        self.running = True        
         
     def load(self):
         """Load a program into memory."""
@@ -18,28 +27,30 @@ class CPU:
         address = 0
 
         # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
-
-
+        if len(sys.argv) < 2:
+            print(TRED + 'File to open is: "examples/mult.ls8"', ENDC)
+            sys.exit()
+        try: 
+            with open(sys.argv[1]) as file:
+                for instruction in file:
+                    inst = instruction.split("#")[0]
+                    if inst == '':
+                        continue
+                    if inst[0] == '1' or inst [0] == '0':
+                        num = inst[:8]
+                        self.ram[address] = int(inst, 2)
+                        address += 1
+        except FileNotFoundError:
+            print(f'{sys.argv[0]}: {sys.argv[1]} not found.')    
+  
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         #elif op == "SUB": etc
+        if op == "MULT":
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -71,33 +82,28 @@ class CPU:
     
     def run(self):
         """Run the CPU."""
-        memory = [0] * len(self.ram)
         pc = self.pc
         HLT = self.HLT
         LDI = self.LDI
+        PRN = self.PRN
+        MULT = self.MULT
+        running = self.running
         
-        while pc < len(memory):
-            command = (self.ram_read(pc))          
-            operand_a = self.ram_read(self.pc + 1)
-            operand_b = self.ram_read(self.pc + 2)
-            memory[pc] = bin(command)[2:]
-            pc += 1
-        
-        for instruction in memory:            
-            if instruction == HLT:
-                print("Program halted.")
-                return
-            if instruction == LDI:
-                print(operand_b)
-                
-          
-      
-        
-       
-                                  
-        
-       
-
-cpu = CPU()
-cpu.load()
-cpu.run()
+        while running:
+            command = bin(self.ram_read(pc))[2:] 
+            operand_a = self.ram_read(pc + 1)
+            operand_b = self.ram_read(pc + 2)
+            
+            add_to_counter = (int(command, 2) >> 6) + 1
+              
+            if command == LDI:
+                self.reg[operand_a] = operand_b
+            elif command == MULT:
+                self.alu("MULT", operand_a, operand_b)
+            elif command == PRN:
+                print(TGREEN + str(self.reg[operand_a]) + ENDC, end=', => ' )
+            elif command == HLT:
+                print(TYELLOW + "Program halted." + ENDC)
+                running = False
+              
+            pc += add_to_counter
