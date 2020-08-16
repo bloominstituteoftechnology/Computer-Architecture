4eda41bf805a483f289eec25c4053be2ec56952f
Shun Chiang <yuushamenma@gmail.com>
2020-08-16 16:48:08 -0500

    finished day 1

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,15 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.reg = [0]*8
+        self.ram = [0]*256
+        self.pc = 0
 
     def load(self):
         """Load a program into memory."""
@@ -18,27 +21,32 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
+
+    def ram_read(self, address):
+        return self.ram[address]
+
+    def ram_write(self, address, value):
+        self.ram[address] = value
 
     def trace(self):
         """
@@ -48,8 +56,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -62,4 +70,20 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        self.load()
+        while self.pc < len(self.ram):
+            command = self.ram[self.pc]
+            HLT = 0b00000001
+
+            if command == HLT:
+                break
+
+            if command == 0b10000010:
+                self.ram_write(self.ram[self.pc+1], self.ram[self.pc+2])
+                self.pc += 2
+
+            if command == 0b01000111:
+                print(self.ram_read(self.ram[self.pc+1]))
+                self.pc += 1
+
+            self.pc += 1
