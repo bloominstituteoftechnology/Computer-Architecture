68e949b072d53492cbedfd6bc384a62645b5435c
David Horstman <davidzilla27@gmail.com>
2020-08-17 11:58:33 -0700

    Done I think

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,15 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0
 
     def load(self):
         """Load a program into memory."""
@@ -18,25 +21,30 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
+    def ram_read(self, addr):
+        return self.ram[addr]
+
+    def ram_write(self, addr, val):
+        self.ram[addr] = val
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +56,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -62,4 +70,32 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        def HLT():
+            return False
+
+        def LDI():
+            self.pc += 1
+            reg = self.ram[self.pc]
+            self.pc += 1
+            val = self.ram[self.pc]
+            self.reg[reg] = val
+            self.pc += 1
+            return True
+
+        def PRN():
+            self.pc += 1
+            reg = self.ram[self.pc]
+            print(self.reg[reg])
+            self.pc += 1
+            return True
+
+        instructions = {
+            0x01: HLT,
+            0x82: LDI,
+            0x47: PRN,
+        }
+
+        while True:
+            ir = self.ram[self.pc]
+            if not instructions[ir]():
+                break
