afd50266b26e6120238100be76bacc68931b0314
johnkirtley <kirtleyj16@gmail.com>
2020-08-17 16:11:59 -0700

    Day 1 Complete

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,21 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0
+
+    def ram_read(self, val):
+        return self.ram[val]
+
+    def ram_write(self, val, address):
+        self.ram[val] = address
 
     def load(self):
         """Load a program into memory."""
@@ -18,25 +27,24 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +56,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -62,4 +70,27 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+
+        HLT = 0b00000001
+        LDI = 0b10000010
+        PRN = 0b01000111
+
+        active = True
+
+        while active:
+            IR = self.ram_read(self.pc)
+
+            operationOne = self.ram_read(self.pc + 1)
+            operationTwo = self.ram_read(self.pc + 2)
+
+            if IR == HLT:
+                self.pc += 1
+                active = False
+
+            elif IR == LDI:
+                self.reg[operationOne] = operationTwo
+                self.pc += 3
+
+            elif IR == PRN:
+                print(self.reg[operationOne])
+                self.pc += 2
