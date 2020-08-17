ed1be4444bc56a49355a87ffd96bde79f5a9cafb
Alexander Besse <alexanderjoeb@hotmail.com>
2020-08-17 18:42:45 -0500

    cpu.py/run - COMPLETED LDP,PRN,HLT & WORKING ON FILE READ FROM CLI ARGS

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -6,8 +6,16 @@ class CPU:
     """Main CPU class."""
 
     def __init__(self):
-        """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0 # Program Counter (PC), the index into memory of the currently-executing instruction
+        
+
+    def ram_read(self, MAR):
+        return self.ram[MAR]
+
+    def ram_write(self, MAR, MDR): # Memory Address Register (MAR) / Memory Data Register (MDR)
+        self.ram[MAR] = MDR
 
     def load(self):
         """Load a program into memory."""
@@ -60,6 +68,42 @@ class CPU:
 
         print()
 
-    def run(self):
+    def run(self, args=None):
         """Run the CPU."""
-        pass
+        
+        if not args or args[0] == '' or not args[0]: print("Error: Could not find file to run.")
+        
+        with open(args[0]) as file:
+            lines = file.readlines()
+            print(lines)
+            for line in lines:
+                line = line.split(' # ')[0].strip()
+                print(f'> {line}')
+
+        running = True
+
+        ir = {
+             0b10000010: 'LDI',
+             0b01000111: 'PRN',
+             0b00000001: 'HLT'
+        }
+
+        while running:
+            i = self.ram[self.pc]
+            ins = ir[i]
+
+            if ins == 'LDI':
+                # Preform LDI Action.
+                reg_num = self.ram_read(self.pc + 1)
+                value = self.ram_read(self.pc + 2)
+                self.reg[reg_num] = [value]
+                self.pc += 3
+            elif ins == 'PRN':
+                # Preform PRN Action.
+                reg_num = self.ram_read(self.pc + 1)
+                print(self.reg[reg_num])
+                self.pc += 2
+            elif ins == 'HLT':
+                # Preform HLT.
+                running = False
+            else: print(f'Unknown Instruction Code: {i}')
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -8,4 +8,4 @@ from cpu import *
 cpu = CPU()
 
 cpu.load()
-cpu.run()
+cpu.run(args=sys.argv[1:])
