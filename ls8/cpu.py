8e49e2a4d2f3e18bbb2798fdf8f5fa5b8d81ca27
GwenStacey <gwen.c.purcell@gmail.com>
2020-08-16 20:08:31 -0600

    Completed sprint challenge

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,31 +7,32 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
+        #R0 - R7
+        #R0 is for flags
+        #R7 is for stack pointer
+        self.reg = [0]*8
         #Initial tracker for commands
-        self.pc = 0
-        self.reg = [None]*8
-        self.ram = [0]*256
-
+        self.pc = self.reg[7]
+        #emulated memory
+        self.ram = [None]*256
+        self.running = True
+    
     def load(self):
-        """Load a program into memory."""
-
         address = 0
-
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        try:
+            with open(sys.argv[1]) as file:
+                for line in file:
+                    comment_split = line.split('#')
+                    possible_num = comment_split[0]
+                    if possible_num == '':
+                        continue
+                    if possible_num[0] == '1' or possible_num[0] == '0':
+                        num = possible_num[:8]
+                        #print(num)
+                        self.ram[address] = int(num,2)
+                        address+=1
+        except FileNotFoundError:
+            print(f'{sys.argv[0]}: {sys.argv[1]} not found!')
 
     def ram_write(self, MAR, MDR):
         self.reg[MAR] = MDR
@@ -45,7 +46,20 @@ class CPU:
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
+            self.pc+=3
         #elif op == "SUB": etc
+        elif op == "MUL":
+            print(self.reg[reg_a]*self.reg[reg_b])
+            self.pc+=3
+        elif op == "CMP":
+            if reg_a == reg_b:
+                self.reg[0] = 0b00000001
+            elif reg_a < reg_b:
+                self.reg[0] = 0b00000100
+            elif reg_a > reg_b:
+                self.reg[0] = 0b00000010
+            self.pc+=3
+        
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -87,12 +101,57 @@ class CPU:
         LDI = 0b10000010
         PRN = 0b01000111
         HLT = 0b00000001
-        while True:
+        MUL = 0b10100010
+        CMP = 0b10100111
+        JMP = 0b01010100
+        JNE = 0b01010110
+        JEQ = 0b01010101
+        while self.running:
             command = self.ram[self.pc]
             if command == LDI:
+                print("LDI")
                 self.ldi()
+                
             if command == PRN:
+                print("PRN")
                 self.prn()
+                
+            if command == MUL:
+                print("MUL")
+                reg_a = self.reg[self.ram[self.pc+1]]
+                reg_b = self.reg[self.ram[self.pc+2]]
+                self.alu("MUL", reg_a, reg_b)
+                
+            if command == CMP:
+                print("CMP")
+                reg_a = self.reg[self.ram[self.pc+1]]
+                reg_b = self.reg[self.ram[self.pc+2]]
+                self.alu("CMP", reg_a, reg_b)
+                
+            if command == JMP:
+                print("JMP")
+                jmp_address = self.ram[self.pc+1]
+                self.pc = self.reg[jmp_address]
+                
+            if command == JNE:
+                print("JNE")
+                jmp_address = self.ram[self.pc+1]
+                if self.reg[0] is 0:
+                    self.pc = self.reg[jmp_address]
+                else:
+                    self.pc+=2
+
+            if command == JEQ:
+                print("JEQ")
+                jmp_address = self.ram[self.pc+1]
+                if self.reg[0] == 0b00000001:
+                    self.pc = self.reg[jmp_address]
+                else:
+                    self.pc+=2
+
             if command == HLT:
-                False
+                print("HLT")
+                self.running = False
+
+
 
