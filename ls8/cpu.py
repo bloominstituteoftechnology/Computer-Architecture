9cdceada635845383d0e3cedb906169e91345426
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-08-17 14:02:23 -0700

    print8

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,26 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.registers = [0] * 8
+        self.memory = [0] * 256
+        self.pc = 0
+        self.machine_codes = {
+            "HLT": 0b00000001,
+            "LDI": 0b10000010,
+            "PRN": 0b01000111,
+            "MUL": 0b10100010,
+            "PUSH": 0b01000101,
+            "POP": 0b01000110,
+            "CALL": 0b01010000,
+            "RET": 0b00010001,
+            "ADD": 0b10100000,
+        }
 
     def load(self):
         """Load a program into memory."""
@@ -16,27 +30,43 @@ class CPU:
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010,  # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111,  # PRN R0
+        #     0b00000000,
+        #     0b00000001,  # HLT
+        # ]
+        # for instruction in program:
+        #     self.memory[address] = instruction
+        #     address += 1
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        with open(sys.argv[1], "r") as program:
+            for instruction in program:
+                # print(instruction)
+                if "#" in instruction:
+                    instruction = instruction.split()[0]
+                    if instruction == "#":
+                        continue
+                else:
+                    instruction = instruction.replace("\n", "")
+                self.memory[address] = int(instruction, 2)
+                address += 1
 
+    def ram_read(self, idx):
+        return self.memory[idx]
+
+    def ram_write(self, idx, user_input):
+        self.memory[idx] = user_input
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -46,20 +76,43 @@ class CPU:
         from run() if you need help debugging.
         """
 
-        print(f"TRACE: %02X | %02X %02X %02X |" % (
-            self.pc,
-            #self.fl,
-            #self.ie,
-            self.ram_read(self.pc),
-            self.ram_read(self.pc + 1),
-            self.ram_read(self.pc + 2)
-        ), end='')
+        print(
+            f"TRACE: %02X | %02X %02X %02X |"
+            % (
+                self.pc,
+                # self.fl,
+                # self.ie,
+                self.ram_read(self.pc),
+                self.ram_read(self.pc + 1),
+                self.ram_read(self.pc + 2),
+            ),
+            end="",
+        )
 
         for i in range(8):
-            print(" %02X" % self.reg[i], end='')
+            print(" %02X" % self.registers[i], end="")
 
         print()
 
     def run(self):
         """Run the CPU."""
-        pass
+        running = True
+        while running:
+            ir = self.ram_read(self.pc)
+
+            if ir == self.machine_codes["HLT"]:  # HLT
+                # shuts off program
+                running = False
+                self.pc += 1
+
+            elif ir == self.machine_codes["PRN"]:  # PRINT_REG
+                reg_num = self.memory[self.pc + 1]
+                print(self.registers[reg_num])
+                self.pc += 2
+
+            elif ir == self.machine_codes["LDI"]:
+                reg_num = self.memory[self.pc + 1]
+                val = self.memory[self.pc + 2]
+                self.registers[reg_num] = val
+                self.pc += 3
+        # self.trace()
