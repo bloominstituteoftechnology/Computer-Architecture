115f4699a48537b3b477ed88b33b732479c02eb7
Henry <henrybsms@outlook.com>
2020-08-18 17:22:43 -0400

    MVP

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,87 +2,55 @@
 
 import sys
 
-# Instructions
-LDI = 0b10000010 
-PRN = 0b01000111    # Print
-HLT = 0b00000001    # Halt
-MUL = 0b10100010    # Multiply
-ADD = 0b10100000    # Addition
-PUSH = 0b01000101   # Push in stack
-POP = 0b01000110    # Pop from stack
-
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        self.ram = [0] * 256
-        self.reg = [0] * 8
-        self.reg[7] = 0xF4
-        self.pc = 0
-        self.running = True
+        self.pc = 0 #add properties for any internal registers you need,
+        self.reg = [0] * 8  #holds 8 general purpose registers
+        self.ram = [0] * 256 #holds 256 bytes of memory
 
-    def ram_read(self, address):
-        return self.ram[address]
 
-    def ram_write(self, address, value):
-        self.ram[address] = value
-    
-    def hlt(self):
-        self.running = False
-        self.pc += 1
-    
-    def ldi(self, reg_num, value):
-        self.reg[reg_num] = value
-        self.pc += 3
+    def ram_read(self, value):
+        return self.ram[value]
+        # reads and returns the value stored there.
 
-    def prn(self, reg_num):
-        print(self.reg[reg_num])
-        self.pc += 2
-    
+
+
+    def ram_write(self,value,addr):
+        self.ram[value] = addr
+        #should accept a value to write, and the address to write it to
+
+
     def load(self):
         """Load a program into memory."""
-        # # hardcoded:
-        # address = 0
-        # program = [
-        #     # From print8.ls8
-        #     0b10000010, # LDI R0,8
-        #     0b00000000,
-        #     0b00001000,
-        #     0b01000111, # PRN R0
-        #     0b00000000,
-        #     0b00000001, # HLT
-        # ]
-        # for instruction in program:
-        #     self.ram[address] = instruction
-        #     address += 1
 
-        # load an .ls8 file given the filename passed in as an argument
-        file_name = sys.argv[1]
-        try:
-            address = 0
-            with open(file_name) as file:
-                for line in file:
-                    split_line = line.split('#')[0]
-                    command = split_line.strip()
+        address = 0
 
-                    if command == '':
-                        continue
+        # For now, we've just hardcoded a program:
 
-                    instruction = int(command, 2)
-                    self.ram[address] = instruction
-                    address += 1
-        except FileNotFoundError:
-            print(f'{sys.argv[0]}: {sys.argv[1]} file was not found')
-            sys.exit()
+        program = [
+            # From print8.ls8
+            0b10000010, # LDI R0,8
+            0b00000000,
+            0b00001000,
+            0b01000111, # PRN R0
+            0b00000000,
+            0b00000001, # HLT
+        ]
+
+        for instruction in program:
+            self.ram[address] = instruction
+            address += 1
+
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        elif op == "MUL":
-            self.reg[reg_a] *= self.reg[reg_b]
+        #elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -108,58 +76,30 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        # load the instructions file
-        self.load()
-        while self.running:
-            instruction_register = self.ram[self.pc]
-            reg_a = self.ram[self.pc+1]
-            reg_b = self.ram[self.pc+2]
+        HLT = 0b00000001
+        LDI = 0b10000010
+        PRN = 0b01000111
 
-            if instruction_register == HLT:
-                self.hlt()
-            
-            elif instruction_register == LDI:
-                self.ldi(reg_a, reg_b)
-            
-            elif instruction_register == PRN:
-                self.prn(reg_a)
-            
-            elif instruction_register == MUL:
-                self.alu("MUL", reg_a, reg_b)
-                self.pc += 3
-            
-            elif instruction_register == PUSH:
-                # decrement stack pointer
-                self.reg[7] -= 1
+        running = True
 
-                # get the value from a given register
-                reg_a = self.ram[self.pc+1]
-                value = self.reg[reg_a]
+        while running:
+            instruction = self.ram_read(self.pc)
+            opr_a = self.ram_read(self.pc + 1)
+            opr_b = self.ram_read(self.pc + 2)
 
-                # put it on the stack pointer address
-                sp = self.reg[7]
-                self.ram[sp] = value
+            if instruction == HLT:
+                running = False
+                self.pc +=1
 
-                # increment pc
+
+            elif instruction == LDI:
+                self.reg[opr_a] = opr_b
+                self.pc += 3 
+
+            elif instruction == PRN:
+                print(self.reg[opr_a])
                 self.pc += 2
 
-            elif instruction_register == POP:
-                # get the tack pointer
-                sp = self.reg[7]
-
-                # get register number to put value in
-                reg_a = self.ram[self.pc+1]
-
-                # use stack pointer to get the value
-                value = self.ram[sp]
-                # put the value into a given register
-                self.reg[reg_a] = value
-
-                # increment stack pointer
-                self.reg[7] += 1
-                # increment program counter
-                self.pc += 2
-            
             else:
-                print(f"Instruction number {self.pc} not recognized!")
-                self.pc += 1
+                print(f"bad input: {bin(instruction)}")
+                running = False
