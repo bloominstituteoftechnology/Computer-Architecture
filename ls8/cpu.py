3dbf08f7464861be7098da6bb82c13e1c798fd2f
Christine Fletcher <chrismis79@gmail.com>
2020-08-17 21:39:45 -0500

    day 1 MVP met

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,16 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.pc = 0
+        self.register = [0] * 8
+        self.running = True
 
     def load(self):
         """Load a program into memory."""
@@ -18,25 +22,24 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +51,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -60,6 +63,36 @@ class CPU:
 
         print()
 
+    def ram_read(self, address):
+        return self.ram[address]
+
+    def ram_write(self, value, address):
+        self.ram[address] = value
+
     def run(self):
         """Run the CPU."""
-        pass
+        prog = {
+            'LDI': 0b10000010,
+            'PRN': 0b01000111,
+            'HLT': 0b00000001,
+            'ADD': 0b10100000,
+            'MUL': 0b10100010
+        }
+        while self.running:
+            IR = self.ram_read(self.pc)
+            operandA = self.ram_read(self.pc + 1)
+            operandB = self.ram_read(self.pc + 2)
+
+            if IR == prog['HLT']:
+                self.running = False
+
+            elif IR == prog['LDI']:
+                address = operandA
+                value = operandB
+                self.register[address] = value
+                self.pc += 3
+            elif IR == prog['PRN']:
+                address = operandA
+                value = self.register[address]
+                print(value)
+                self.pc += 2
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -7,5 +7,6 @@ from cpu import *
 
 cpu = CPU()
 
+
 cpu.load()
 cpu.run()
