dc55a4cabbd355635102b07244c676fd3c725b22
Jon Nguyen <jon.mail@hotmail.com>
2020-08-19 11:41:41 -0700

    push pop working

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,10 +7,15 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        self.reg = [0] * 8 # Variablesfor cpu to use with instructions
+        self.reg = [0] * 7 # Variables for cpu to use with instructions. Register
+        self.reg.append(0b11110100) # F4 in binary, used for sp
+        self.sp = self.reg[7] # Stack Pointer, address to top stack, or F4
         self.ram = [None] * 256 # memory slots to keep track of variables
-        self.pc = 0 # pointer to track operations in register
+        self.pc = 0 # pointer to track operations in register. Program Counter
         self.running = True
+        self.branchtable = {
+
+        }
 
     def ram_read(self, MAR): # Memory Address Register
         # get the value at ram address
@@ -25,19 +30,6 @@ class CPU:
         """Load a program into memory.
         Bonus: check to make sure the user has put a command line argument
          where you expect, and print an error and exit if they didn’t."""
-
-
-        # # For now, we've just hardcoded a program:
-
-        # program = [
-        #     # From print8.ls8
-        #     0b10000010, # LDI R0,8
-        #     0b00000000,
-        #     0b00001000,
-        #     0b01000111, # PRN R0
-        #     0b00000000,
-        #     0b00000001, # HLT
-        # ]
         try:
             with open(argument, "r") as f:
                program = f.read()
@@ -52,9 +44,20 @@ class CPU:
         except FileNotFoundError:
             print("Error with file from command line")
 
+        # # For now, we've just hardcoded a program:
+
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
     def alu(self, op, reg_a, reg_b):
-        """ALU operations."""
+        """ALU operations. arithmetic logic unit"""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
@@ -101,6 +104,8 @@ class CPU:
         PRN = 0b01000111
         HLT = 0b00000001
         MUL = 0b10100010
+        PUSH = 0b01000101
+        POP = 0b01000110
 
 
         while self.running:
@@ -108,6 +113,7 @@ class CPU:
             operand_a = self.ram_read(self.pc + 1)
             operand_b = self.ram_read(self.pc + 2)
             # print(self.reg)
+            # self.trace()
             if IR == HLT:
                 self.running = False
 
@@ -120,6 +126,31 @@ class CPU:
                 print(self.reg[operand_a])
                 self.pc += 2
             
+            elif IR == PUSH: # Decrement Stack Pointer, take register value into RAM
+                # Decrement stack pointer
+                self.sp += -1
+
+                # Get register number
+                reg_num = operand_a
+                value = self.reg[reg_num]
+
+                # The stack pointer tells us where to put the value
+                self.ram[self.sp] = value
+
+                # move the program counter when done
+                self.pc += 2
+
+            elif IR == POP: # Pop value at top of stack from RAM into given register, increment
+                # value to pop comes from where the stack pointer is
+                value = self.ram[self.sp]
+
+                # pop it into register
+                self.reg[operand_a] = value
+                self.sp += 1
+
+                # move the program counter when done
+                self.pc += 2
+                
             elif IR == MUL: # multiply the next two
                self.alu("MUL", operand_a, operand_b)
                self.pc += 3
@@ -127,5 +158,5 @@ class CPU:
             else:
                 print(f"This whole court is outta order!")
         
-        # self.trace()
+
 
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -7,6 +7,10 @@ from cpu import *
 
 cpu = CPU()
 # I think we will have to do sys.argv[1] or something into the load 
+if len(sys.argv) < 2:
+    print("Please enter filename after module in command line")
+    print("Example: python ls8.py examples/mult.ls8")
+    sys.exit()
 filename = sys.argv[1]
 cpu.load(filename)
 cpu.run()
