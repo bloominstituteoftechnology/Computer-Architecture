cbcfc0524800670995382bd78990f2c0dd84c846
Jon Nguyen <jon.mail@hotmail.com>
2020-08-19 12:13:22 -0700

    branch table optimization

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -14,6 +14,12 @@ class CPU:
         self.pc = 0 # pointer to track operations in register. Program Counter
         self.running = True
         self.branchtable = {
+            0b10000010: self.LDI,
+            0b01000111: self.PRN, 
+            0b00000001: self.HLT, 
+            0b10100010: self.MUL, 
+            0b01000101: self.PUSH,
+            0b01000110: self.POP
 
         }
 
@@ -99,8 +105,8 @@ class CPU:
         00000000
         00000001 # HLT
         """
-        # should be similar to lecture
-        LDI = 0b10000010 # These are instructions we need to translate
+
+        LDI = 0b10000010 
         PRN = 0b01000111
         HLT = 0b00000001
         MUL = 0b10100010
@@ -113,50 +119,47 @@ class CPU:
             operand_a = self.ram_read(self.pc + 1)
             operand_b = self.ram_read(self.pc + 2)
             # print(self.reg)
-            # self.trace()
-            if IR == HLT:
-                self.running = False
-
-            elif IR == LDI: # next value is the pc, one after is the value to set register to
-               self.reg[operand_a] = operand_b
-               self.pc += 3
-               
-
-            elif IR == PRN: # psuedo instr. Print value stored at register
-                print(self.reg[operand_a])
-                self.pc += 2
-            
-            elif IR == PUSH: # Decrement Stack Pointer, take register value into RAM
-                # Decrement stack pointer
-                self.sp += -1
-
-                # Get register number
-                reg_num = operand_a
-                value = self.reg[reg_num]
-
-                # The stack pointer tells us where to put the value
-                self.ram[self.sp] = value
-
-                # move the program counter when done
-                self.pc += 2
-
-            elif IR == POP: # Pop value at top of stack from RAM into given register, increment
-                # value to pop comes from where the stack pointer is
-                value = self.ram[self.sp]
-
-                # pop it into register
-                self.reg[operand_a] = value
-                self.sp += 1
-
-                # move the program counter when done
-                self.pc += 2
-                
-            elif IR == MUL: # multiply the next two
-               self.alu("MUL", operand_a, operand_b)
-               self.pc += 3
+            #self.trace()
+            if IR in self.branchtable:
+                self.branchtable[IR](operand_a, operand_b)
 
             else:
                 print(f"This whole court is outta order!")
+                self.running = False
+    def HLT(self, operand_a, operand_b):
+        self.running = False
+
+    def LDI(self, operand_a, operand_b): 
+        # opA is the value, opB is the value to set
+        self.reg[operand_a] = operand_b
+        self.pc += 3
+               
+
+    def PRN(self, operand_a, operand_b): 
+        # opA is the reg index to print
+        print(self.reg[operand_a])
+        self.pc += 2
+            
+    def PUSH(self, operand_a, operand_b):
+        # Decrement stack pointer
+        self.sp += -1
+        # opA tells us which register value, stack pointer is where to store
+        self.ram[self.sp] = self.reg[operand_a]
+        # move the program counter when done
+        self.pc += 2
+
+    def POP(self, operand_a, operand_b):
+        # sp is the value you want to put into the register
+        value = self.ram[self.sp]
+        # opA is the register you want to store into
+        self.reg[operand_a] = value
+        self.sp += 1
+        self.pc += 2
+
+    def MUL(self, operand_a, operand_b): # multiply the next two
+        self.alu("MUL", operand_a, operand_b)
+        self.pc += 3
+
         
 
 
