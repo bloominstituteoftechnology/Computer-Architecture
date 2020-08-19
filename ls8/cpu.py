6b6efc91632acd09c960e607945c2134b8010727
Mark Poggi <markp@mac.com>
2020-08-19 12:14:39 -0700

    Added push, pop and return functionality

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -10,6 +10,7 @@ class CPU:
         self.pc = 0
         self.ram = [0] * 256
         self.reg = [0] * 8
+        self.SP = self.reg[7]
 
     def load(self):
         """Load a program into memory."""
@@ -95,24 +96,52 @@ class CPU:
             ir = self.ram[self.pc] # Instruction Register - internal part of CPU that holds a value.  Special purpose part of CPU.
 
             if ir == 130: # LDI instruction
-                reg_num = self.ram[self.pc+1]
-                value = self.ram[self.pc+2]
+                reg_num = self.ram_read(self.pc+1)
+                value = self.ram_read(self.pc+2)
                 self.reg[reg_num] = value
                 self.pc += (ir >> 6) + 1 # using bit shifting to determine length of instruction set.
             
             elif ir == 71: # PRN instruction
-                reg_num = self.ram[self.pc+1]
+                reg_num = self.ram_read(self.pc+1)
                 print(self.reg[reg_num])
                 self.pc += (ir >> 6) + 1
 
             elif ir == 1: # HLT instruction
                 running = False
 
-            elif ir == 162:
-                num1 = self.ram[self.pc+1]
-                num2 = self.ram[self.pc+2]
+            elif ir == 162: # MUL
+                num1 = self.ram_read(self.pc+1)
+                num2 = self.ram_read(self.pc+2)
                 print(self.alu("MUL", num1, num2))
                 self.pc += (ir >> 6) + 1
+
+            elif ir == 69: # PUSH
+                self.SP -= 1 # decriment SP (stack pointer)
+                reg_num = self.ram_read(self.pc + 1)   # get value from register
+                value = self.reg[reg_num] # we want to push this value
+                top_of_stack_addr = self.SP # get top of stack address
+                self.ram[top_of_stack_addr] = value # store value at top of the stack
+                self.pc += (ir >> 6) + 1 # increment PC
+
+            elif ir == 70: # POP
+                top_of_stack_addr = self.SP
+                reg_num = self.ram_read(self.pc+1)
+                self.reg[reg_num] = self.ram[top_of_stack_addr]
+                self.SP += 1
+                self.pc += (ir >> 6) + 1
+
+            elif ir == 17: # RET
+                # Pop the value from the top of the stack and store it in the PC
+                self.pc = self.ram_read(self.SP)
+                self.SP += 1
+
+            # CALL 
+            # Calls a subroutine (function) at the address stored in the register.
+            # - The address of the instruction directly after CALL is pushed onto the stack. This allows us to return to where we left off when the subroutine finishes executing.
+            # - The PC is set to the address stored in the given register. We jump to that location in RAM and execute the first instruction in the subroutine. The PC can move forward or backwards from its current location.
+
+            elif ir == 80: # CALL
+                pass
 
             else:
                 self.pc += (ir >> 6) + 1
