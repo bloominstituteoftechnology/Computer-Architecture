ee92f2e571314ad8fcca57fc705972dcfbfcbf4d
bobbygondola <60513315+bobbygondola@users.noreply.github.com>
2020-08-21 01:47:18 -0400

    refactor

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -40,8 +40,6 @@ class CPU:
                 else:
                     self.ram[address] = int(line, 2)
                     address += 1
-                
-                print(line)
 
         # for instruction in program:
         #     self.ram[address] = instruction
@@ -105,25 +103,26 @@ class CPU:
         MUL = 0b10100010 # Multiply 
         PUSH = 0b01000101 # Push
         POP = 0b01000110 # Pop
-        CALL = 0B01010000
-        RET = 0B00010001
+        CALL = 0b01010000 # Call
+        RET = 0b00010001 # RET
+        ADD = 0b10100000 # ADD - DO IT!
         SP = 7 # Stack Pointer
-        self.reg[SP] = 244 # top of stack, also 0xf4
+        self.reg[SP] = 244  # 0xf4 top of stack, also 0xf4
         
         running = True
         
         while running:
             ir = self.ram[self.pc] # Instruction Register
             
-            reg_a = self.ram[self.pc + 1]   # first register value
-            reg_b = self.ram[self.pc + 2]   # second register value
-            
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+    
             if ir == HLT:
                 running = False
                 self.pc += 1
 
             elif ir == LDI:
-            #Set the value of a register to an integer.
+            # Set the value of a register to an integer.
                 reg_num = self.ram[self.pc + 1]
                 value = self.ram[self.pc + 2]
                 self.reg[reg_num] = value
@@ -160,35 +159,31 @@ class CPU:
 
                 self.pc += 2
             
-            # The CALL instruction
-            elif self.ram[self.pc] == CALL:
-                # Add the pushing & must decrement the SP
-                self.reg[7] -= 1
-                SP = self.reg[7]
-                # get the address of the next instruction for when subroutine is complete
-                addr_next_instruction = self.pc + 2
-                # then put that address on the stack
-                self.ram[SP] = addr_next_instruction
-                # then find the reg to call from and the address in that reg
-                reg_to_call = self.ram[self.pc + 1]
-                addr_to_go_to = self.reg[reg_to_call]
-                # and set the pc to that address
-                self.pc = addr_to_go_to
+            # CALL: The PC is set to the address stored in the given register. 
+            # We jump to that location in RAM and execute the first instruction in the subroutine. 
+            # The PC can move forward or backwards from its current location.
 
-            # The RET instruction
-            elif self.ram[self.pc] == RET:
-                # This is an instance of pop so don't decrement the SP
-                SP = self.reg[7]
-                # Get the address at the top of the stack
-                addr_to_pop = self.ram[SP]
-                # and set PC to that address
-                self.pc = addr_to_pop
-                # After pop then increment the SP
-                self.reg[7] += 1
-        
-            # else:
-            #     print(f"Invalid instruction {ir} at address {pc}")
-            #     sys.exit(1)
+            elif ir == CALL:
+                SP -= 1
+                self.ram[SP] = self.pc + 2 
+                regnum = operand_a
+                self.pc = self.reg[regnum]
+
+            # RET: Pop the value from the top of the stack and store it in the PC.
+
+            elif ir == RET:
+                self.pc = self.ram[SP]
+                SP += 1
+
+            # ADD: Add the value in two registers and store the result in registerA.
+
+            elif ir == ADD:
+                self.reg[operand_a] += self.reg[operand_b]
+                self.pc += 3
+            
+            else:
+                print(f"Invalid instruction {ir} at address {self.pc}")
+                sys.exit(1)
             
             # read value where the pointer is pointing
             
