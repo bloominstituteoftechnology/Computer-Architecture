7efced4501fb97d3ad9c6564cdaadb8cbb27d76c
Francois Chery <swa_isthecreator@Francoiss-MBP.fios-router.home>
2020-09-04 21:14:24 -0400

    working on my cpu/run function

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -6,8 +6,10 @@ class CPU:
     """Main CPU class."""
 
     def __init__(self):
-        """Construct a new CPU."""
-        pass
+        self.pc = 0 # program counter
+        self.registers = [0] * 8 # example R0 - R7
+        self.ram = [0] * 256 # list
+        self.running = True
 
     def load(self):
         """Load a program into memory."""
@@ -16,14 +18,18 @@ class CPU:
 
         # For now, we've just hardcoded a program:
 
+        HALT = 0b00000001
+        PRNT = 0b01000111
+        LDI = 0b10000010
+
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            LDI, # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            PRNT, # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            HALT,
         ]
 
         for instruction in program:
@@ -62,4 +68,17 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+
+        while self.running: 
+            instruction = self.ram[self.pc]
+
+            if instruction == self.ram_read(self.load()):
+                self.running = False
+        
+
+    def ram_read(self, address):
+        if address in self.ram:
+            return self.ram[address]
+
+    def ram_write(self, address, value):
+        self.ram[address] = value
