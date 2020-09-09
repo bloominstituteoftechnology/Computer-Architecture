befb1fc0001807e114b4840e073a8d9354e4b307
Charlie <charles.j.montoya@gmail.com>
2020-09-09 19:21:18 -0400

    Day 1 Completed, working on Day 2

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,34 +1,58 @@
 """CPU functionality."""
 
+import os.path
+from os import path
+
 import sys
+
 
 class CPU:
     """Main CPU class."""
 
+    ## NOT SURE WHAT TO DO ABOUT LISTING INVENTORY ##
+
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = {}
+        self.registers = [0] * 8
+        
 
     def load(self):
         """Load a program into memory."""
+
+        file_name = sys.argv[1]
+
+        program = []
+
+        f = open(file_name, "r")
+
+        for line in f:
+
+            program.append(line[:8])
+
+        f.close()
+
+        print("Program from Load: ", program)
 
         address = 0
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
         for instruction in program:
+            
             self.ram[address] = instruction
             address += 1
+
 
 
     def alu(self, op, reg_a, reg_b):
@@ -62,4 +86,54 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        
+        running = True
+
+        pc = 0
+
+
+        while running:
+            # Read Line by Line from Memory
+            instruction = self.ram[pc]
+
+            if instruction == '10000010':
+                
+                reg_location = self.ram[pc + 1]
+
+                num = self.ram[pc + 2]
+
+                self.registers[int(reg_location)] = num
+
+                pc += 3
+
+            elif instruction == '01000111':
+
+                reg_location = self.ram[pc + 1]
+
+                print(self.registers[int(reg_location)])
+
+                pc += 2
+
+            elif instruction == '00000001':
+
+                running = False
+
+                pc += 1
+            
+            elif instruction == '10100010':
+
+                print("BICHO")
+
+                reg_location_1 = self.ram[pc + 1]
+
+                reg_location_2 = self.ram[pc + 2]
+
+                ## Multiply Numbers Here ##
+
+                pc += 3
+        
+            else: 
+                
+                print(f"Unknown Instruction: {instruction}")
+
+                sys.exit(1) 
