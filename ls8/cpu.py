8bb2ca9f0777201c74fd59a1cbfa606e543a8155
Michael Lowe <mrlowememphis@yahoo.com>
2020-09-09 22:31:44 -0500

    Day one complete

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -4,12 +4,52 @@ import sys
 
 class CPU:
     """Main CPU class."""
+    ops = {
+        0x00: "NOP",   # no-op
+        0x01: "HLT",   # halt; exit
+        0x50: "CALL",  # call subroutine at register address
+        0x11: "RET",   # return from subroutine
+        0x52: "INT",   # interrupt
+        0x13: "IRET",  # return from interrupt
+        0x54: "JMP",   # jump to address in given reg
+        0x55: "JEQ",   # if == flag true, jump to address in given reg
+        0x56: "JNE",   # if E flag is clear, jump to address in given reg
+        0x57: "JGT",   # if >, jump to address in given reg
+        0x58: "JLT",   # if <
+        0x59: "JLE",   # if < or ==
+        0x5A: "JGE",   # if > or ==
+        0x82: "LDI",   # set value of register to integer
+        0x83: "LD",    # loads regA w/ value at mem address stored in regB
+        0x84: "ST",    # store value in regB at address in regA
+        0x45: "PUSH",  # push value in given reg onto stack
+        0x46: "POP",   # pop value at top of stack into given reg
+        0x47: "PRN",   # print numeric value stored in given reg
+        0x48: "PRA",   # print alpha char value stored in given reg
+        0xA0: "ADD",   # addition (ALU)
+        0xA1: "SUB",   # subtract (ALU)
+        0xA2: "MUL",   # multiply (ALU)
+        0xA3: "DIV",   # division (ALU)
+        0xA4: "MOD",   # modulo (ALU)
+        0x65: "INC",   # increment (ALU)
+        0x66: "DEC",   # decrement (ALU)
+        0xA7: "CMP",   # comparison (ALU)
+        0xA8: "AND",   # bitwise-and (ALU)
+        0x69: "NOT",   # bitwise-not (ALU)
+        0xAA: "OR",    # bitwise-or (ALU)
+        0xAB: "XOR",   # XOR (ALU)
+        0xAC: "SHL",   # bitshift left (ALU)
+        0xAD: "SHR",   # bitshift right (ALU)
+    }
 
     def __init__(self):
         """Construct a new CPU."""
         # pass
         self.ram = [0] * 256
-        self.cpu_reg = [0] * 8
+        # self.cpu_reg = [0] * 8
+        self.reg = [0] * 8
+        self.pc = 0
+        self.dispatch = {
+        }
 
     def ram_read(self, address):
         return self.ram[address]
@@ -71,8 +111,27 @@ class CPU:
     def run(self):
         """Run the CPU."""
         # pass
-        ir = self.pc
+        # ir = self.pc
+        ir = self.ram_read(self.pc)  # instruction register
 
-        (operand_a, operand_b) = (self.ram_read(ir+1), self.ram_read(ir+2))
+        operand_a = self.ram_read(self.pc + 1)
+        operand_b = self.ram_read(self.pc + 2)
+        operation = CPU.ops.get(ir)
+
+        if operation is None:
+            return
+        elif operation == "HLT":
+            return
+        elif operation == "LDI":
+            self.reg[operand_a] = operand_b
+        elif operation == "PRN":
+            print(self.reg[operand_a])
+
+        self.pc += (ir >> 6) + 1
+        self.run()
+
+
+
+        # (operand_a, operand_b) = (self.ram_read(ir+1), self.ram_read(ir+2))
         
-        #
+     
