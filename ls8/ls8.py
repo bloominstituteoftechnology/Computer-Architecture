dac0891a8907d10a2eae103f16439b8dbde259ea
unknown <C.Berumen51@gmail.com>
2020-09-10 16:21:57 -0700

    working on day 2

--- ls8/ls8.py
+++ ls8/ls8.py
@@ -7,5 +7,13 @@ from cpu import *
 
 cpu = CPU()
 
-cpu.load()
+if len(sys.argv) < 2:
+    print("Please pass a second filename: python3 file_one.py file_two.py")
+    sys.exit()
+
+file_name = sys.argv[1]
+
+cpu.load(file_name)
 cpu.run()
+
+
