9311ff4093991c1b9fa75b508caf73ec0fdd38bd
Skyler Suzuki <skylersuzuk1@gmail.com>
2020-09-17 13:26:04 -0700

    CALL and RET tested and finished.

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -6,9 +6,12 @@ SP = 7
 LDI = 0b10000010
 PRN = 0b01000111
 MUL = 0b10100010
+ADD = 0b10100000
 HLT = 0b00000001
 PUSH = 0b01000101
 POP = 0b01000110
+CALL = 0b01010000
+RET = 0b00010001
 
 class CPU:
     """Main CPU class."""
@@ -23,9 +26,12 @@ class CPU:
         self.branchtable[LDI] = self.ldi
         self.branchtable[PRN] = self.prn
         self.branchtable[MUL] = self.mul
+        self.branchtable[ADD] = self.add
         self.branchtable[HLT] = self.hlt
         self.branchtable[POP] = self.pop
         self.branchtable[PUSH] = self.push
+        self.branchtable[CALL] = self.call
+        self.branchtable[RET] = self.ret
 
     def load(self, filename):
         """Load a program into memory."""
@@ -111,9 +117,11 @@ class CPU:
         while True:
             ir = self.ram_read(self.pc)
             self.branchtable[ir]()
-            number_of_operands = (ir & 0b11000000) >> 6
-            how_far_to_move_pc = number_of_operands + 1
-            self.pc += how_far_to_move_pc
+            inst_sets_pc = (ir >> 4) & 1
+            if inst_sets_pc != 1:
+                number_of_operands = (ir & 0b11000000) >> 6
+                how_far_to_move_pc = number_of_operands + 1
+                self.pc += how_far_to_move_pc
 
     def ldi(self):
         operand_a = self.ram_read(self.pc + 1)
@@ -132,15 +140,24 @@ class CPU:
         operand_b = self.ram_read(self.pc + 2)
         self.alu("MUL", operand_a, operand_b)
 
-    def push(self):
-        # Decrement SP
-        self.reg[SP] -= 1
+    def add(self):
+        operand_a = self.ram_read(self.pc + 1)
+        operand_b = self.ram_read(self.pc + 2)
+        self.alu("ADD", operand_a, operand_b)
 
+    def push(self):
         # Get the reg num to push
         reg_num = self.ram_read(self.pc + 1)
 
         # Get the value to push
         value = self.reg[reg_num]
+
+        # Push the value to stack
+        self.push_value(value)
+
+    def push_value(self, value):
+        # Decrement SP
+        self.reg[SP] -= 1
 
         # Copy the value to the SP address
         top_of_stack_addr = self.reg[SP]
@@ -150,14 +167,41 @@ class CPU:
         # Get reg to pop into
         reg_num = self.ram_read(self.pc + 1)
 
-        # Get the top of stack addr
-        top_of_stack_addr = self.reg[SP]
-
 		# Get the value at the top of the stack
-        value = self.ram_read(top_of_stack_addr)
+        value = self.pop_value()
 
 		# Store the value in the register
         self.reg[reg_num] = value
 
-		# Increment the SP
-        self.reg[SP] += 1
+    def pop_value(self):
+	    # Get the top of stack addr
+	    top_of_stack_addr = self.reg[SP]
+
+	    # Get the value at the top of the stack
+	    value = self.ram_read(top_of_stack_addr)
+
+	    # Increment the SP
+	    self.reg[SP] += 1
+
+	    return value
+
+    def call(self):
+        # Compute the return addr
+        return_addr = self.pc + 2
+
+        # Push return addr on stack
+        self.push_value(return_addr)
+
+        # Get the value from the operand reg
+        reg_num = self.ram_read(self.pc + 1)
+        value = self.reg[reg_num]
+
+        # Set the pc to that value
+        self.pc = value
+
+    def ret(self):
+        # Pop the value off the stack
+        value = self.pop_value()
+
+        # Set the pc to that value
+        self.pc = value
