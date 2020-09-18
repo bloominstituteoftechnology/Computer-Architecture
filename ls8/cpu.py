0c218751e1984ff206b87ba3c38904cad66b385e
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-09-18 13:35:57 -0400

    Day 1 MVP

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,35 @@
 
 import sys
 
+# Program Actions
+LDI = 0b10000010 # LOAD IMMEDIATE
+PRN = 0b01000111 # PRINT
+HLT = 0b00000001 # HALT
+
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256 # 256 bytes of memory
+        self.reg = [0] * 8 # General Purpose Registers 0 - 7
+        self.pc = 0 # Program Counter
+
+    # access the RAM inside the CPU object
+    # MAR (Memory Address Register) - contains the address that is 
+        # being read / written to
+    def ram_read(self, MAR):
+        # accepts the address to read and return the value stored there
+        print(self.ram[MAR])
+        return self.ram[MAR]
+    
+    # access the RAM inside the CPU object
+    # MDR (Memory Data Register) - contains the data that was read or 
+        # the data to write
+    def ram_write(self, MDR, MAR):
+        # accepts a vale to write and the address to write it to
+        self.ram[MAR] = MDR
 
     def load(self):
         """Load a program into memory."""
@@ -62,4 +85,36 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+
+        running = True
+
+        while running:
+            # read the memory address (MAR) that's stored in register PC (self.pc)
+            # store the result in IR (Instruction Register)
+            IR = self.ram_read(self.pc)
+
+            # ram_read() - read bytes at PC + 1 and PC + 2 from RAM into variables 
+            # operand_a and operand_b
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+
+            # HALT
+            if IR == HLT:
+                # Exit Loop
+                running = False
+                # Update PC
+                self.pc += 1
+
+            # PRINT
+            elif IR == PRN:
+                # Print Reg
+                print(self.reg[operand_a])
+                # Update PC
+                self.pc += 2
+
+            # LDI = LOAD IMMEDIATE
+            elif IR == LDI:
+                # Assign value to Reg Key
+                self.reg[operand_a] = operand_b
+                # Update PC
+                self.pc += 3
