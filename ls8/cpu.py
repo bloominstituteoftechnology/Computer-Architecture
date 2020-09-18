e905cd9e245616d5fd628a466a80b66953c63ce9
Skyler Suzuki <skylersuzuk1@gmail.com>
2020-09-18 11:36:07 -0700

    sctest.ls8 completed and tested.

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -12,6 +12,10 @@ PUSH = 0b01000101
 POP = 0b01000110
 CALL = 0b01010000
 RET = 0b00010001
+CMP = 0b10100111
+JEQ = 0b01010101
+JNE = 0b01010110
+JMP = 0b01010100
 
 class CPU:
     """Main CPU class."""
@@ -22,6 +26,7 @@ class CPU:
         self.reg = [0] * 8
         self.pc = 0
         self.reg[SP] = 0xF4
+        self.fl = 0
         self.branchtable = {}
         self.branchtable[LDI] = self.ldi
         self.branchtable[PRN] = self.prn
@@ -32,6 +37,10 @@ class CPU:
         self.branchtable[PUSH] = self.push
         self.branchtable[CALL] = self.call
         self.branchtable[RET] = self.ret
+        self.branchtable[CMP] = self.cmp
+        self.branchtable[JEQ] = self.jeq
+        self.branchtable[JNE] = self.jne
+        self.branchtable[JMP] = self.jmp
 
     def load(self, filename):
         """Load a program into memory."""
@@ -88,6 +97,19 @@ class CPU:
             self.reg[reg_a] += self.reg[reg_b]
         elif op == "MUL":
             self.reg[reg_a] *= self.reg[reg_b]
+        elif op == "CMP":
+            # `FL` bits: `00000LGE`
+            if self.reg[reg_a] == self.reg[reg_b]:
+                # Set 'E' flag to 1
+                self.fl = 0b00000001
+            else:
+                self.fl = 0
+            # elif self.reg[reg_a] < self.reg[reg_b]:
+            #     # Set 'L' flag to 1
+            #     self.fl &= 0b00000100
+            # elif self.reg[reg_a] > self.reg[reg_b]:
+            #     # Set 'G' flag to 1
+            #     self.fl &= 0b00000010
         #elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
@@ -116,9 +138,9 @@ class CPU:
         """Run the CPU."""
         while True:
             ir = self.ram_read(self.pc)
-            self.branchtable[ir]()
+            jmp_jne_jeq = self.branchtable[ir]()
             inst_sets_pc = (ir >> 4) & 1
-            if inst_sets_pc != 1:
+            if inst_sets_pc != 1 or jmp_jne_jeq == False:
                 number_of_operands = (ir & 0b11000000) >> 6
                 how_far_to_move_pc = number_of_operands + 1
                 self.pc += how_far_to_move_pc
@@ -205,3 +227,38 @@ class CPU:
 
         # Set the pc to that value
         self.pc = value
+
+    def cmp(self):
+        operand_a = self.ram_read(self.pc + 1)
+        operand_b = self.ram_read(self.pc + 2)
+        self.alu("CMP", operand_a, operand_b)
+
+    def jeq(self):
+        # If `equal` flag is set (true)
+        if self.fl == 1:
+            # Jump to the address stored in the given register
+            self.pc = self.jmp_to()
+            return True
+        else:
+            return False
+
+    def jne(self):
+        # If `E` flag is clear (false, 0)
+        if self.fl != 1:
+            # jump to the address stored in the given register
+            self.pc = self.jmp_to()
+            return True
+        else:
+            return False
+
+    def jmp(self):
+        # Jump to the address stored in the given register.
+        # Set the `PC` to the address stored in the given register.
+        self.pc = self.jmp_to()
+        return True
+
+    def jmp_to(self):
+        reg_num = self.ram_read(self.pc + 1)
+        value = self.reg[reg_num]
+
+        return value
