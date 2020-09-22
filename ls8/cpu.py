32a7b3965c161567cb4bf3f14d6cf7d2e8f72ec2
Crystal Knight <crystalknightcodes@gmail.com>
2020-09-22 10:02:07 -0400

    Update cpu.py
    Created CPU core program.

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,19 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        # Internal Registers
+        self.pc     = 0b00000000
+        self.ir     = NOP
+        self.mar    = 0b00000000
+        self.mdr    = 0b00000000
+        self.fl     = 0b00000000  # 00000LGE - only last 3 bits matter
+
+        # General Purpose Registers
+        self.reg = [0] * 8  # this is the CPU's register
+
+        # Memory
+        self.ram = [0] * 265 # size of the computer's memory
+
 
     def load(self):
         """Load a program into memory."""
@@ -62,4 +74,41 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        while True:
+            self.ir = self.ram_read(self.pc)
+
+            if self.ir == HLT:
+                print("Halting program. Goodbye.")
+                break
+            elif self.ir == LDI:
+                self.reg[self.ram_read(self.pc+1)] = self.ram_read(self.pc+2)
+                self.pc += 3
+            elif self.ir == PRN:
+                print(self.reg[self.ram_read(self.pc+1)])
+                self.pc +=2
+            else:
+                print("Unknown command. Halting program. Goodbye.")
+                break
+
+    def ram_read(self, address = None):
+        if address is not None:
+            self.mar = address
+
+        self.mdr = self.ram[self.mar]
+        return self.mdr
+
+    def ram_write(self, address = None, value = None):
+        if address is not None:
+            self.mar = address
+        if value is not None:
+            self.mdr = value
+
+        self.ram[self.mar] = self.mdr
+
+ 
+if __name__ == "__main__":
+    cpu = CPU()
+    print(len(cpu.ram))
+
+    cpu.ram_write()
+    print(cpu.ram[3])
