6319a5f447c0ec3f9b16431f24cd14d6be58b12d
David Wright <davidtwright89@gmail.com>
2020-10-02 00:20:57 -0400

    Refactored cpu.py and ls8.py so the program can be specified on the command line

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -52,23 +52,9 @@ class CPU:
         else:
             print(f"Error: Attempted to write to memory address: {mar}, which is outside of the memory bounds.")
 
-    def load(self):
+    def load(self, program):
         """Load a program into memory."""
-
         address = 0
-
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
         for instruction in program:
             self.ram[address] = instruction
             address += 1
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -2,10 +2,36 @@
 
 """Main."""
 
+argv_err_msg = """
+Error: You must specify a file name as a command line argument.
+This file should contain the binary instruction set you want the CPU to run.
+
+    Ex: python3 ls8.py examples/print8.ls8
+"""
+
 import sys
 from cpu import *
 
 cpu = CPU()
 
-cpu.load()
+# python3 ls8.py examples/print8.ls8
+if len(sys.argv) != 2:
+    print(argv_err_msg)
+    sys.exit(1)
+
+program = []
+file_name = sys.argv[1]
+try:
+    with open(file_name) as f:
+        for line in f:
+            num = line.split("#")[0]
+            try:
+                program.append(int(num, 2))
+            except:
+                continue
+except:
+    print('Could not find file named: {file_name}')
+    sys.exit(1)
+
+cpu.load(program)
 cpu.run()
