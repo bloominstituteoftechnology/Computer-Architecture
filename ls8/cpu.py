e0c89e4170bdd1403fe871b1614391cf42137993
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-10-06 18:15:17 -0400

    Day 2

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -23,27 +23,39 @@ class CPU:
     def ram_write(self, address, val):
         self.ram[address] = val
 
-    def load(self):
-        """Load a program into memory."""
+#    def load(self):
+#        """Load a program into memory."""
+#
+#        address = 0
+#
+#        # For now, we've just hardcoded a program:
+#
+#        program = [
+#            # From print8.ls8
+#            0b10000010, # LDI R0,8
+#            0b00000000,
+#            0b00001000,
+#            0b01000111, # PRN R0
+#            0b00000000,
+#            0b00000001, # HLT
+#        ]
+#
+#        for instruction in program:
+#            self.ram[address] = instruction
+#            address += 1
 
-        address = 0
-
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
-
+    def load(self, filename):
+            """Load a program into memory."""
+            address = 0
+            with open(filename) as fp:
+                for line in fp:
+                    comment_split = line.split("#")
+                    num = comment_split[0].strip()
+                    if num == '':  # ignore blanks
+                        continue
+                    val = int(num, 2)
+                    self.ram_write(val, address)
+                    address += 1
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
