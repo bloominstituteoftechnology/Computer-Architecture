8dfd3f313f50eb068aa8e09f9074134981507666
Tusk617 <59673124+Tusk617@users.noreply.github.com>
2020-10-13 18:05:10 -0400

    Got yesterdays work to output properly, and updated the load function to take different files into account

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,6 +2,9 @@
 
 import sys
 
+HLT = 0b00000001
+PRN = 0b01000111
+LDI = 0b10000010
 class CPU:
     """Main CPU class."""
 
@@ -12,7 +15,7 @@ class CPU:
         self.pc = 0
     
     def ram_read(self, address):
-        print(self.ram[address])
+        return self.ram[address]
     def ram_write(self, value, address):
         self.ram[address] = value
 
@@ -21,28 +24,52 @@ class CPU:
 
         address = 0
 
+        try:
+            with open(sys.argv[1]) as f:
+                #can be interpreted as "for instruction in program"
+                for line in f:
+                    line = line.strip()
+
+                    #accounting for any comments or blank lines
+                    if line == '' or line[0] == '#'
+                    continue
+
+                    try:
+                        str_value = line.split("#")[0]
+                        value = int(str_value, 10)
+
+                    except ValueError:
+                        print(f"Invalid number: {str_value}")
+                        sys.exit()
+                    
+                    ram[address] = value
+                    address += 1
+        except FileNotFoundError:
+            print(f"File not found!: {sys.argv[1]}")
+            sys.exit(0)
+
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
 
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
+            self.register[reg_a] += self.register[reg_b]
         #elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
@@ -63,31 +90,36 @@ class CPU:
         ), end='')
 
         for i in range(8):
-            print(" %02X" % self.reg[i], end='')
+            print(" %02X" % self.register[i], end='')
 
         print()
 
     def run(self):
         """Run the CPU."""
-        ir = self.ram[self.pc]
-        # register_pointer = 0
-
-        operand_a = self.ram_read(self.pc + 1)
-        operand_b = self.ram_read(self.pc + 2)
         
         running = True
 
         while running is True:
+            ir = self.ram[self.pc]
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
 
-            if ir == 0b00001000:
-                print(self.register[self.pc])
-            elif ir == 0b00000001:
+            if ir == HLT:
                 #HLT
+                self.pc += 1
                 running = False
-            elif ir == 0b10000010:
+            elif ir == LDI:
                 #LDI
-                self.register[self.pc] = 8
-            elif ir == 0b01000111:
+                reg_num = operand_a
+                value = operand_b
+                self.register[reg_num] = value
+                self.pc += 3
+            elif ir == PRN:
                 #PRN
-                print(self.register[self.pc])
+                reg_num = operand_a
+                print(self.register[reg_num])
+                self.pc += 2
+            else:
+                print("Unknown instruction")
+                sys.exit(0)
 
