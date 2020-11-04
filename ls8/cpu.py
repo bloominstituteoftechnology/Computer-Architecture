e88fc6111a488a89ef07db69fa7ae9888a0aa774
Your name <a.m.verdine@gmail.com>
2020-11-04 11:20:22 -0500

    Refactor code

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,6 +1,12 @@
 """CPU functionality."""
 
 import sys
+import os.path
+
+HLT = 0b00000001
+LDI = 0b10000010
+PRN = 0b01000111
+MUL = 0b10100010
 
 class CPU:
     """Main CPU class."""
@@ -25,6 +31,15 @@ class CPU:
         # Initialize the Stack Pointer
         # SP points at the value at the op of the stack (most recently pushed), or at address F4 if the stack is empty
         self.reg[7] = 0xF4 # 244 # int('F4', 16)
+
+        # Property wrapper for stack pointers
+        @property
+        def sp(self):
+            return self.reg[7]
+
+        @sp.setter
+        def sp(self, a):
+            self.reg[7] = a & 0xFF
 
 
     def ram_read(self, mar):
@@ -94,34 +109,54 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        running = True
+        # running = True
         
-        while running:
-            # Fetch the next instruction
+        # while running:
+        #     # Fetch the next instruction
+        
+        while not self.halted
             self.ir = self.ram_read(self.pc)
             operand_a = self.ram_read(self.pc + 1)
             operand_b = self.ram_read(self.pc + 2)
 
-            # Decode instruction
-            binary_ir = bin(self.ir)[2:].zfill(8)
-            operand_count = int(binary_ir[2], 2)
-            is_ALU_operation = binary_ir[2] == '1'
-            instruction_does_set_pc = binary_ir[3] == '1'
-            instruction_id = int(binary_ir[4:], 2)
+            # # Decode instruction
+            # binary_ir = bin(self.ir)[2:].zfill(8)
+            # operand_count = int(binary_ir[2], 2)
+            # is_ALU_operation = binary_ir[2] == '1'
+            # instruction_does_set_pc = binary_ir[3] == '1'
+            # instruction_id = int(binary_ir[4:], 2)
 
-            #Increment the program counter
-            self.pc += (1 + operand_count)
+            # #Increment the program counter
+            # self.pc += (1 + operand_count)
 
-            # Execute instruction
-            if self.ir == int('00000001', 2): # HLT
-                running - False
-            elif self.ir == int('10000010', 2): # LDI
-                self.reg[operand_a] = operand_b
-            elif self.ir == int('01000111', 2): # PRN
-                print(self.reg[operand_a])
-            else:
-                print(f"Error: Could not execute instruction: {bin(self.ir)[2:].zfill(8)}")
-                sys.exit(1)
+            # # Execute instruction
+            # if self.ir == int('00000001', 2): # HLT
+            #     running - False
+            # elif self.ir == int('10000010', 2): # LDI
+            #     self.reg[operand_a] = operand_b
+            # elif self.ir == int('01000111', 2): # PRN
+            #     print(self.reg[operand_a])
+            # else:
+            #     print(f"Error: Could not execute instruction: {bin(self.ir)[2:].zfill(8)}")
+            #     sys.exit(1)
+            self.execute_instruction(operand_a, operand_b)
+
+    def execute_instruction(self, operand_a, operand_b):
+        if self.ir == HLT:
+            self.halted = True
+            self.pc += 1
+
+        elif self.ir == LDI:
+            self.reg[operand_a] = operand_b
+            self.pc += 3
+
+        elif self.ir == PRN:
+            print(self.eg[operand_a])
+            self.pc += 2
+
+        else:
+            print(f"Error: Could not execute intsruction: {self.ir}")
+            sys.exit(1)
 
 
 
