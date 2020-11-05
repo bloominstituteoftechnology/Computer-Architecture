d8d76f9ccb06ca73582a1c3e2cab6878f3550407
Your name <a.m.verdine@gmail.com>
2020-11-04 20:02:06 -0500

    Recfactor cpu.py and ls8.py

--- ls8/ls8.py
+++ ls8/ls8.py
@@ -8,4 +8,24 @@ from cpu import *
 cpu = CPU()
 
 cpu.load()
+#  python3 ls8.py examples/print8.ls8
+if len(sys.argv) != 2:
+    print(argv_err_msg)
+    sys.exit(1)
+
+program = []
+file_name = sys.argv[1]
+try:
+    with open(file_name) as f:
+        for line in f:
+            num = line.split("#")[0]
+            try:
+                program.append(int(num, 2))
+            except:
+                continue
+except:
+    print(f'Could not find file named: {file_name}')
+    sys.exit(1)
+
+cpu.load(program)
 cpu.run()
