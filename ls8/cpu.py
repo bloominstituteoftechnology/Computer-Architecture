69f2771cac4610d8789fbf065851e8ad09a5d54a
Your name <a.m.verdine@gmail.com>
2020-11-08 19:55:06 -0500

    Update run() in cpu.py // Test program using sctest.ls8

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -175,14 +175,21 @@ class CPU:
         """Run the CPU."""
         
         while not self.halted:
+
+            # Fetch instructions
             self.ir = self.ram_read(self.pc)
-            operand_a = self.ram_read(self.pc + 1)
-            operand_b = self.ram_read(self.pc + 2)
 
-            if not self.instruction_sets_pc():
-                self.pc += self.instruction_size()
+            # Execute instructions
+            if self.ir in self.branchtable:
+                self.branchtable[self.ir]()
+            else:
+                print(f"Error: Could not find instruction: {self.ir} in branch table.")
+                sys.exit(1)
 
-            self.execute_instruction(operand_a, operand_b)
+            # Conditionally increment program counter
+            if not self.instruction_sets_pc:
+                self.pc += self.instruction_size
+  
 
     def execute_instruction(self, operand_a, operand_b):
         if self.ir in self.branchtable:
