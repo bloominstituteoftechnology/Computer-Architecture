d72da52599831dfc0066afa865d2e4b31763d80a
kmk028 <kmk028@users.noreply.github.com>
2020-12-06 20:57:27 -0800

    Getting an error with MUL

--- asm/mult.asm
+++ asm/mult.asm
@@ -2,7 +2,7 @@
 #
 # Expected output: 72
 
-LDI R0,8
+LDI R0,8 
 LDI R1,9
 MUL R0,R1
 PRN R0
--- ls8/cpu.py
+++ ls8/cpu.py
@@ -5,6 +5,7 @@ import sys
 HLT = 0b00000001
 LDI = 0b10000010
 PRN = 0b01000111
+MUL = 0b10100010
 
 class CPU:
     def __init__(self):
@@ -21,33 +22,34 @@ class CPU:
     def ram_write(self,val,addr):
         self.ram[addr] = val
 
-    def load(self):
+    def load(self,filename):
         """Load a program into memory."""
 
         address = 0
+       # program = []
 
-        # For now, we've just hardcoded a program:
+        with open(filename) as f:
+            for line in f:
+                comment_split = line.split("#")
+                maybe_binary_number = comment_split[0].strip()
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+                try:
+                    x = int(maybe_binary_number, 2)
+                   # program.append(x)
+                    
+                except:
+                    continue
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+           # for instruction in program:
+                self.ram[address] = x
+                address += 1
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
-
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        elif op == "MUL": 
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -71,6 +73,10 @@ class CPU:
 
         print()
 
+
+    def num_of_operands(self, instruction_to_execute):
+        return ((instruction_to_execute >> 6) & 0b11) + 1
+
     def run(self):
         """Run the CPU."""
         
@@ -89,6 +95,9 @@ class CPU:
             elif instruction_to_excecute == HLT:
                 self.running = False
                 self.pc +=1 
+            elif instruction_to_excecute == MUL:
+                self.alu(instruction_to_excecute,operand_a,operand_b)
+                self.pc += self.num_of_operands(instruction_to_excecute)
             else:
                 print ("idk what to to")
                 pass
