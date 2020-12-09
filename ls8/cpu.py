84c68b93f99eb303d5746cd0150a1d66aa65fd6e
BenHall-7 <benjaminjahall@gmail.com>
2020-12-09 00:22:22 -0500

    Implement interrupt handling

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,16 +1,26 @@
 """CPU functionality."""
 
 import sys
+import time
 import traceback
+
+IM = 5
+IS = 6
+SP = 7
 
 HLT = 0b0001
 LDI = 0b0010
+ST = 0b0100
 PUSH = 0b0101
 POP = 0b0110
 PRN = 0b0111
+PRA = 0b1000
 
 CALL = 0b0000
 RET = 0b0001
+INT = 0b0010
+IRET = 0b0011
+JMP = 0b0100
 
 ALU_ADD = 0b0000
 ALU_SUB = 0b0001
@@ -34,14 +44,28 @@ class CPU:
         """Construct a new CPU."""
         self.ram = [0] * 256
         self.reg = [0] * 8
+        self.reg[SP] = 0xf4
         self.pc = 0
-        self.sp = 0xf4
+        self.fl = 0
+        # these aren't mentioned in the spec
+        # but there didn't seem to be a suitable place to put them
+        self.start_time = 0
+        self.time = 0
 
     def ram_read(self, addr):
         return self.ram[addr]
 
     def ram_write(self, val, addr):
         self.ram[addr] += val
+    
+    def push(self, val):
+        self.reg[SP] -= 1
+        self.ram[self.reg[SP]] = val
+
+    def pop(self):
+        val = self.ram[self.reg[SP]]
+        self.reg[SP] += 1
+        return val
 
     def load(self, program):
         """Load a program into memory."""
@@ -110,7 +134,33 @@ class CPU:
 
     def run(self):
         try:
+            self.start_time = time.time()
             while True:
+                self.reg[IS] = 0
+                # timer interrupt status
+                self.time = time.time()
+                if self.time >= self.start_time + 1:
+                    self.start_time += ((self.time - self.start_time) // 1)
+                    self.reg[IS] |= 1 # timer interrupt bit
+                
+                # keyboard interrupt status
+                # TODO
+
+                # check interrupts
+                maskedInterrupts = self.reg[IS] & self.reg[IM]
+                for bit in range(8):
+                    if (maskedInterrupts >> bit) & 1:
+                        self.reg[IS] & ~(1 << bit)
+                        self.push(self.pc)
+                        self.push(self.fl)
+                        for reg in range(7):
+                            self.push(self.reg[reg])
+                        # go to interrupt handler from interrupt address table
+                        self.pc = self.ram[0xf8 + bit]
+                        # disable further interrupts
+                        self.reg[IM] = 0
+                        break
+
                 ir = self.ram_read(self.pc)
                 
                 self.pc += 1
@@ -127,13 +177,29 @@ class CPU:
                 if is_alu:
                     self.alu(instruction, next(operands), next(operands))
                 elif sets_pc:
-                    if instruction == CALL:
-                        self.sp -= 1
-                        self.ram[self.sp] = self.pc
+                    def call():
+                        self.push(self.pc)
                         self.pc = self.reg[next(operands)]
-                    elif instruction == RET:
-                        self.pc = self.ram[self.sp]
-                        self.sp += 1
+                    def ret(): self.pc = self.pop()
+                    def jmp(): self.pc = self.reg[next(operands)]
+                    def int_(): self.reg[IS] |= 1 << self.reg[next(operands)]
+                    def iret():
+                        for reg in reversed(range(7)):
+                            self.reg[reg] = self.pop()
+                        self.fl = self.pop()
+                        self.pc = self.pop()
+                        self.reg[IM] = ~0
+
+                    INSTRUCTIONS = {
+                        CALL: call,
+                        RET: ret,
+                        JMP: jmp,
+                        INT: int_,
+                        IRET: iret
+                    }
+
+                    if instruction in INSTRUCTIONS:
+                        INSTRUCTIONS[instruction]()
                     else:
                         raise Exception(f"UNRECOGNIZED INSTRUCTION: {instruction:b}")
                 elif instruction == HLT:
@@ -144,18 +210,21 @@ class CPU:
                         b = next(operands)
                         self.reg[a] = b
                     def prn(): print(self.reg[next(operands)])
-                    def push():
-                        self.sp -= 1
-                        self.ram[self.sp] = self.reg[next(operands)]
-                    def pop():
-                        self.reg[next(operands)] = self.ram[self.sp]
-                        self.sp += 1
+                    def pra(): print(chr(self.reg[next(operands)]))
+                    def push(): self.push(self.reg[next(operands)])
+                    def pop(): self.reg[next(operands)] = self.pop()
+                    def st():
+                        a = next(operands)
+                        b = next(operands)
+                        self.ram[self.reg[a]] = self.reg[b]
                     
                     INSTRUCTIONS = {
                         LDI: ldi,
                         PRN: prn,
+                        PRA: pra,
                         PUSH: push,
                         POP: pop,
+                        ST: st,
                     }
 
                     if instruction in INSTRUCTIONS:
