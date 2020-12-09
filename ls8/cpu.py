d1343edd6c74f50b50b626444f4f9b3c704aca84
jarparrish <jarparrish@gmail.com>
2020-12-08 21:15:23 -0700

    mvp

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -6,6 +6,8 @@ HLT = 0b00000001
 LDI = 0b10000010
 PRN = 0b01000111
 MULT = 0b10100010
+PUSH = 0b01000101
+POP = 0b01000110
 
 class CPU:
     """Main CPU class."""
@@ -20,6 +22,8 @@ class CPU:
         #self.reg = [0, 0, 0, 0, 0, 0, 0, 0xF4]
         self.reg = [0] * 8
         self.reg[7] = 0xF4
+        self.stack_pointer = 7
+        self.memory = []
         """ self.HLT = 0b00000001
         self.LDI = 0b10000010
         self.PRN = 0b01000111 """
@@ -32,7 +36,7 @@ class CPU:
 
     def load(self, args):
         """Load a program into memory."""
-
+        # My implementation
         program_code = []
         
         try:
@@ -50,7 +54,28 @@ class CPU:
             print("File Not Found")
             
         print("program code!!!!!!!!!!!", program_code)
+        stack_space = 256 - len(program_code)
+        memory = program_code + [0] * stack_space
         
+        self.reg[self.stack_pointer] = len(memory) - 1
+        print('memory', memory)
+        #########################################################################################
+        ############################################################################### From Class
+        """ address = 0
+
+        with open(args) as my_file:
+            for line in my_file:
+                comment_split = line.split("#")
+                maybe_binary_number = comment_split[0]
+                try:
+                    x = int(maybe_binary_number, 2)
+                    self.ram_write(x, address)
+                    address += 1
+                except:
+                    continue """
+
+
+        ##########################################################################################
 
         
 
@@ -68,9 +93,10 @@ class CPU:
             0b00000001, # HLT
         ]
 
-        for instruction in program_code:
+        for instruction in memory:
             self.ram[address] = instruction
             address += 1
+        print(self.ram, 'ram')
 
 
     def alu(self, op, reg_a, reg_b):
@@ -152,7 +178,7 @@ class CPU:
         elif instruction == LDI:
             self.reg[operand_a] = operand_b
             self.pc += 3
-            print('ldi ran')
+            print(f'ldi ran --{operand_b} store in reg{operand_a}')
 
         elif instruction == MULT:
             """ self.reg[operand_a] = self.reg[operand_a] * self.reg[operand_b]
@@ -161,3 +187,23 @@ class CPU:
             self.alu(instruction, operand_a, operand_b)
             self.pc += 3
             print('mult ran')
+        elif instruction == PUSH:
+            self.reg[self.stack_pointer] -= 1
+            reg_to_retrieve_val = operand_a
+            val_in_reg = self.reg[reg_to_retrieve_val]
+            self.ram[self.reg[self.stack_pointer]] = val_in_reg
+            self.pc += 2
+            
+            print('push --- val in ram', self.ram[self.reg[self.stack_pointer]] )
+        elif instruction == POP:
+            reg_to_pop = operand_a
+            
+            self.reg[reg_to_pop] = self.ram[self.reg[self.stack_pointer]]
+            print(f'pop ran --{self.ram[self.reg[self.stack_pointer]]}in reg{reg_to_pop}')
+            self.reg[self.stack_pointer] += 1
+            self.pc += 2
+           
+        ##### MULT FROM CLASS
+        """ elif instruction == MULT:
+            self.reg[operand_a] *= self.reg[operand_b]
+            self.pc += 3 """
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -5,9 +5,17 @@
 import sys
 from cpu import *
 
+# From class
+""" if(len(sys.argv) != 2):
+    print('Please pass two arguments')
+else:
+    console_args = sys.argv[1]
+    cpu = CPU()
+    cpu.load(console_args)
+    cpu.run() """
+##############
+
 console_args = sys.argv[1]
-
 cpu = CPU()
-
 cpu.load(console_args)
 cpu.run()
