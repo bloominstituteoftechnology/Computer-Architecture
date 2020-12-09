bbfef65430a5553d9b59ac7fc5f5394ba2526523
Claudia Maciel <clc_80@yahoo.com>
2020-12-08 18:52:46 -0800

    Un-hardcode the machine code

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -47,28 +47,22 @@ class CPU:
     def ram_write(self, address, val):
         self.ram[address] = val
 
-    def load(self):
+    def load(self, filename):
         """Load a program into memory."""
 
         address = 0
 
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
-
-
+        with open(filename) as fp:
+            for line in fp:
+                comment_split = line.split("#")
+                num = comment_split[0].strip()
+                # ignore blanks
+                if num == "" : 
+                    continue
+                val = int(num, 2)
+                self.ram_write(val, address)
+                address += 1
+        
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
