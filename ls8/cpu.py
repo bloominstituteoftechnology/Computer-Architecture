88e458119d7749e58ff230957f79eb9e2505bbef
Skantastico <Skantastico@gmail.com>
2020-12-11 08:47:52 -0500

    added call, ret, add, got call.ls8 working

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -3,35 +3,41 @@
 import sys
 import os.path
 
-
-HLT  = 0b00000001 # halt code
-LDI  = 0b10000010 # load integer
-PRN  = 0b01000111 # print
-MUL  = 0b10100010 # multiply
-PUSH = 0b01000101 # push stack
-POP  = 0b01000110 # pop stack
-
-
+HLT  = 0b00000001
+LDI  = 0b10000010
+PRN  = 0b01000111
+MUL  = 0b10100010
+PUSH = 0b01000101
+POP  = 0b01000110
+CALL = 0b01010000
+RET  = 0b00010001
+ADD  = 0b10100000
 
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        self.ram = [0] * 256 # ram
-        self.reg = [0] * 8 # registers
-        self.pc = 0 # counter
-        self.ir = 0 # instruction register
-        self.mar = 0 # memory address register
-        self.mdr = 0 # memory data register
-        self.fl = 0 # flag register
+        # 256-byte RAM, each element is 1 byte (can only store integers 0-255)
+        self.ram = [0] * 256
+
+        # R0-R7: 8-bit general purpose registers, R5 = interrupt mask (IM),
+        # R6 = interrupt status (IS), R7 = stack pointer (SP)
+        self.reg = [0] * 8
+
+        # Internal Registers
+        self.pc = 0 # Program Counter: address of the currently executing instruction
+        self.ir = 0 # Instruction Register: contains a copy of the currently executing instruction
+        self.mar = 0 # Memory Address Register: holds the memory address we're reading or writing
+        self.mdr = 0 # Memory Data Register: holds the value to write or the value just read
+        self.fl = 0 # Flag Register: holds the current flags status
         self.halted = False
 
-        # initialize stack pointer
-        self.reg[7] = 0xF4
+        # Initialize the Stack Pointer
+        # SP points at the value at the top of the stack (most recently pushed), or at address F4 if the stack is empty.
+        self.reg[7] = 0xF4 # 244 # int('F4', 16)
 
-        # setup branch table
-
+        # Setup Branch Table
         self.branchtable = {}
         self.branchtable[HLT] = self.execute_HLT
         self.branchtable[LDI] = self.execute_LDI
@@ -39,40 +45,55 @@ class CPU:
         self.branchtable[MUL] = self.execute_MUL
         self.branchtable[PUSH] = self.execute_PUSH
         self.branchtable[POP] = self.execute_POP
+        self.branchtable[CALL] = self.execute_CALL
+        self.branchtable[RET] = self.execute_RET
+        self.branchtable[ADD] = self.execute_ADD
 
-    # stack pointer
+    # Property wrapper for SP (Stack Pointer)
 
     @property
-    def pointer(self):
+    def sp(self):
         return self.reg[7]
 
-    @pointer.setter
-    def pointer(self, a):
+    @sp.setter
+    def sp(self, a):
         self.reg[7] = a & 0xFF
 
+    # Computed Properties
+
+    @property
+    def operand_a(self):
+        return self.ram_read(self.pc + 1)
+
+    @property
+    def operand_b(self):
+        return self.ram_read(self.pc + 2)
+
+    @property
     def instruction_size(self):
         return ((self.ir >> 6) & 0b11) + 1
 
+    @property
     def instruction_sets_pc(self):
         return ((self.ir >> 4) & 0b0001) == 1
+
+    # CPU Methods
 
     def ram_read(self, mar):
         if mar >= 0 and mar < len(self.ram):
             return self.ram[mar]
         else:
-            print(f"Error: Attempted to read memory out of bounds.")
+            print(f"Error: Attempted to read from memory address: {mar}, which is outside of the memory bounds.")
             return -1
 
     def ram_write(self, mar, mdr):
         if mar >= 0 and mar < len(self.ram):
             self.ram[mar] = mdr & 0xFF
         else:
-            print("Error Attempted to write memory out of bounds.")
-
+            print(f"Error: Attempted to write to memory address: {mar}, which is outside of the memory bounds.")
 
     def load(self, file_name):
         """Load a program into memory."""
-
         address = 0
 
         file_path = os.path.join(os.path.dirname(__file__), file_name)
@@ -95,10 +116,7 @@ class CPU:
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-
-        elif op == "MUL":
-            self.reg[reg_a] *=self.reg[reg_b]
-
+        #elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -122,46 +140,59 @@ class CPU:
 
         print()
 
+    # Run Loop
+
     def run(self):
         """Run the CPU."""
-
         while not self.halted:
+
+            # Fetch the next instruction (it is decoded lazily using computed properties)
             self.ir = self.ram_read(self.pc)
-            operand_a = self.ram_read(self.pc + 1)
-            operand_b = self.ram_read(self.pc + 2)
 
-            if not self.instruction_sets_pc():
-                self.pc += self.instruction_size()
+            # Execute the instruction
+            if self.ir in self.branchtable:
+                self.branchtable[self.ir]()
+            else:
+                print(f"Error: Could not find instruction: {self.ir} in branch table.")
+                sys.exit(1)
 
-            self.execute_instruction(operand_a, operand_b)
+            # Increment the program counter if necessary
+            if not self.instruction_sets_pc:
+                self.pc += self.instruction_size
 
-    def execute_instruction(self, operand_a, operand_b):
-        if self.ir in self.branchtable:
-            self.branchtable[self.ir](operand_a, operand_b)
-        else:
-            print(f"Error: Instruction not found.")
-            sys.exit(1)
 
-    # refactored commands
+    # Define the operations to be loaded into the branch table
 
-    def execute_HLT(self, operand_a, operand_b):
+    def execute_HLT(self):
         self.halted = True
 
-    def execute_LDI(self, operand_a, operand_b):
-        self.reg[operand_a] = operand_b
+    def execute_LDI(self):
+        self.reg[self.operand_a] = self.operand_b
 
-    def execute_PRN(self, operand_a, operand_b):
-        print(self.reg[operand_a])
+    def execute_PRN(self):
+        print(self.reg[self.operand_a])
 
-    def execute_MUL(self, operand_a, operand_b):
-        self.reg[operand_a] *= self.reg[operand_b]
+    def execute_MUL(self):
+        self.reg[self.operand_a] *= self.reg[self.operand_b]
 
-    def execute_PUSH(self, operand_a, operand_b):
-        self.pointer -= 1
-        value_in_register = self.reg[operand_a]
-        self.ram[self.pointer] = value_in_register
+    def execute_PUSH(self):
+        self.sp -= 1
+        self.mdr = self.reg[self.operand_a]
+        self.ram_write(self.sp, self.mdr)
 
-    def execute_POP(self, operand_a, operand_b):
-        top_of_stack = self.ram[self.pointer]
-        self.reg[operand_a] = top_of_stack
-        self.pointer += 1
+    def execute_POP(self):
+        self.mdr = self.ram_read(self.sp)
+        self.reg[self.operand_a] = self.mdr
+        self.sp += 1
+
+    def execute_CALL(self):
+        self.sp -= 1
+        self.ram_write(self.sp, self.pc + self.instruction_size)
+        self.pc = self.reg[self.operand_a]
+
+    def execute_RET(self):
+        self.pc = self.ram_read(self.sp)
+        self.sp += 1
+
+    def execute_ADD(self):
+        self.reg[self.operand_a] += self.reg[self.operand_b]
