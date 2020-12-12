5c1a6f9d6b1e73c1b83e865868ef92f130d18199
AbreuOliver <oliverabreu@icloud.com>
2020-12-12 13:27:15 -0500

    feat(cpu.py): Creates 8 general-purpose registers

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -8,6 +8,7 @@ class CPU:
     def __init__(self):
         """Construct a new CPU."""
         self.ram = [0] * 256
+        self.reg = [0] * 8
 
     def load(self):
         """Load a program into memory."""
