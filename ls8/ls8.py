a96f21b686b6a09c1a68029c7c759218f555fd97
AbreuOliver <oliverabreu@icloud.com>
2020-12-12 15:13:21 -0500

    feat(ls8.py): Implements 'load' function

--- ls8/ls8.py
+++ ls8/ls8.py
@@ -7,5 +7,12 @@ from cpu import *
 
 cpu = CPU()
 
-cpu.load()
-cpu.run()
+try:
+    filename = sys.argv[1]
+    with open(filename) as f:
+        cpu.load(filename)
+        cpu.run()
+except IndexError:
+    print("Please provide an input")
+except FileNotFoundError:
+    print(f"Error from {sys.argv[0]}: {sys.argv[1]} not found")
