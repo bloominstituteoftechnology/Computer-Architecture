c38d1a0fe4a3178e2fa1f65fb0df0bce1868a1ff
SFMack <sfcamack@icloud.com>
2020-12-13 20:05:12 -0800

    turning in late work

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,65 +1,129 @@
-"""CPU functionality."""
-
 import sys
 
 class CPU:
-    """Main CPU class."""
+    # Main
+
+    HLT = 0b00000001
+    LDI = 0b10000010
+    PRN = 0b01000111
+    ADD = 0b10100000
+    MUL = 0b10100010
+    PUSH = 0b01000101
+    POP = 0b01000110
+    CALL = 0b01010000
+    RET = 0b00010001
+    CMP = 0b10100111
+    JMP = 0b01010100
+    JEQ = 0b01010101
+    JNE = 0b01010110
 
     def __init__(self):
-        """Construct a new CPU."""
-        pass
+        # Constructing the CPU
+        self.programCounter = 0
+        self.ram = [0] * 256
+        self.register = [0] * 8
+        self.stackPointer = 7
+        self.register[self.stackPointer] = 0xF4
+        self.running = True
+        self.flagsRegister = 0b00000000
+
+    # finds the location in ram based on 'address'
+    def read(self, address):
+        return self.ram[address]
+
+    
+    def write(self, address, value):
+        self.ram[address] = value
 
     def load(self):
-        """Load a program into memory."""
-
         address = 0
 
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
-
-
-    def alu(self, op, reg_a, reg_b):
-        """ALU operations."""
-
-        if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        if len(sys.argv) != 2:
+            print("Wrong number of arguments")
+            sys.exit(1)
+        with open(sys.argv[1]) as f:
+            for line in f:
+                line_split = line.split("#")
+                command = line_split[0].strip()
+                if command == '':
+                    continue
+                else:
+                    self.ram[address] = int(command[:8], 2)
+                    address += 1
+    
+    def alu(self, operation, register_a, register_b):
+        if operation == self.ADD:
+            self.register[register_a] += self.register[register_b]
+            self.programCounter += 3
+        elif operation == self.HLT:
+            self.running = False
+            self.programCounter += 1
+        elif operation == self.LDI:
+            self.register[register_a] = register_b
+            self.programCounter += 3
+        elif operation == self.PRN:
+            print(self.register[register_a])
+            self.programCounter += 2
+        elif operation == self.MUL:
+            self.register[register_a] *= self.register[register_b]
+            self.programCounter += 3
+        elif operation == self.PUSH:
+            registerAddress = self.ram[self.programCounter + 1]
+            value = self.register[registerAddress]
+            self.register[self.stackPointer] -= 1
+            self.ram[self.register[self.stackPointer]] = value
+            self.programCounter += 2
+        elif operation == self.POP:
+            registerAddress = self.ram[self.programCounter + 1]
+            self.register[registerAddress] = self.ram[self.register[self.stackPointer]]
+            self.register[self.stackPointer] += 1
+            self.programCounter += 2
+        elif operation == self.CALL:
+            self.register[self.stackPointer] -= 1
+            self.ram[self.register[self.stackPointer]] = self.programCounter + 2
+            registerNumber = self.ram[self.programCounter + 1]
+            self.programCounter = self.register[registerNumber]
+        elif operation == self.CMP:
+            if self.register[register_a] < self.register[register_b]:
+                self.flagsRegister = 0b00000100
+            elif self.register[register_a] == self.register[register_b]:
+                self.flagsRegister = 0b00000001
+            self.programCounter += 3
+        elif operation == self.JMP:
+            register = self.read(self.programCounter + 1)
+            self.programCounter = self.register[register]
+        elif operation == self.JNE:
+            register = self.read(self.programCounter + 1)
+            if self.flagsRegister & 0b00000001 == False:
+                self.programCounter = self.register[register]
+            else:
+                self.programCounter += 2
+        elif operation == self.JEQ:
+            register = self.read(self.programCounter + 1)
+            if self.flagsRegister & 0b00000001:
+                self.programCounter = self.register[register]
+            else:
+                self.programCounter += 2
         else:
-            raise Exception("Unsupported ALU operation")
+            raise Exception("Unsupported operation")       
 
     def trace(self):
-        """
-        Handy function to print out the CPU state. You might want to call this
-        from run() if you need help debugging.
-        """
-
         print(f"TRACE: %02X | %02X %02X %02X |" % (
-            self.pc,
-            #self.fl,
-            #self.ie,
-            self.ram_read(self.pc),
-            self.ram_read(self.pc + 1),
-            self.ram_read(self.pc + 2)
+            self.programCounter,
+            self.read(self.programCounter),
+            self.read(self.programCounter + 1),
+            self.read(self.programCounter + 2)
         ), end='')
 
         for i in range(8):
-            print(" %02X" % self.reg[i], end='')
+            print(" %02X" % self.register[i], end='')
 
         print()
 
     def run(self):
-        """Run the CPU."""
-        pass
+        while self.running:
+            IR = self.ram[self.programCounter]
+            register_a = self.ram[self.programCounter + 1]
+            register_b = self.ram[self.programCounter + 2]
+
+            self.alu(IR, register_a, register_b)
